9d778637b5910b8bb936592b3a5e8b915a94e1fa
Siraj Razick <siraj@kdemail.net>
2007-06-05 18:39:59 +0000

    ifndef typo fixed now the item can be used
    svn path=/trunk/KDE/kdebase/workspace/lib/plasma/; revision=671898

--- widgets/icon.h
+++ widgets/icon.h
@@ -16,8 +16,8 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#ifndef PUSHBUTTON_H
-#define PUSHBUTTON_H
+#ifndef ICON_H
+#define ICON_H
 
 #include <QtCore/QObject>
 #include <QtGui/QGraphicsTextItem>
