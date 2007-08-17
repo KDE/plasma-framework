c86bf6caeaf130a1fc4bca226e8d3f7d3616b3ad
Percy Leonhardt <percy@eris23.de>
2007-08-17 14:36:48 +0000

    Example for nested layouts
    svn path=/trunk/KDE/kdebase/workspace/libs/plasma/; revision=701173

--- widgets/tests/testLayouts.cpp
+++ widgets/tests/testLayouts.cpp
@@ -13,6 +13,7 @@
 #include "../vboxlayout.h"
 #include "../hboxlayout.h"
 #include "../widget.h"
+#include "../label.h"
 
 int main(int argc, char **argv)
 {
@@ -29,35 +30,38 @@ int main(int argc, char **argv)
     Plasma::VBoxLayout *widgetLayout = new Plasma::VBoxLayout(0);
     widgetLayout->setGeometry( QRectF(0.0, 0.0, 400.0, 700.0) );
 
+    Plasma::HBoxLayout *h1 = new Plasma::HBoxLayout( 0 );
+    Plasma::HBoxLayout *h2 = new Plasma::HBoxLayout( 0 );
+    widgetLayout->addItem( h1 );
+    widgetLayout->addItem( h2 );
+
+    // should be first row
     Plasma::PushButton *pushOne = new Plasma::PushButton("pushbutton one");
-    Plasma::PushButton *pushTwo = new Plasma::PushButton("pushbutton two");
-    Plasma::PushButton *pushThree = new Plasma::PushButton("pushbutton three");
-    Plasma::PushButton *pushFour = new Plasma::PushButton("pushbutton four");
-
-    widgetLayout->addItem(pushOne);
-    widgetLayout->addItem(pushTwo);
-    widgetLayout->addItem(pushThree);
-    widgetLayout->addItem(pushFour);
-
+    h1->addItem(pushOne);
     scene.addItem(pushOne);
-    scene.addItem(pushTwo);
-    scene.addItem(pushThree);
-    scene.addItem(pushFour);
 
     Plasma::LineEdit *editOne = new Plasma::LineEdit;
-    Plasma::LineEdit *editTwo = new Plasma::LineEdit;
-    Plasma::LineEdit *editThree = new Plasma::LineEdit;
-    Plasma::LineEdit *editFour = new Plasma::LineEdit;
-
-    widgetLayout->addItem(editOne);
-    widgetLayout->addItem(editTwo);
-    widgetLayout->addItem(editThree);
-    widgetLayout->addItem(editFour);
-
+    h1->addItem(editOne);
     scene.addItem(editOne);
+
+    Plasma::Label *labelOne = new Plasma::Label( 0 );
+    labelOne->setText( "hello world 1" );
+    h1->addItem(labelOne);
+    scene.addItem(labelOne);
+
+    // should be second row
+    Plasma::PushButton *pushTwo = new Plasma::PushButton("pushbutton two");
+    h2->addItem(pushTwo);
+    scene.addItem(pushTwo);
+
+    Plasma::LineEdit *editTwo = new Plasma::LineEdit;
+    h2->addItem(editTwo);
     scene.addItem(editTwo);
-    scene.addItem(editThree);
-    scene.addItem(editFour);
+
+    Plasma::Label *labelTwo = new Plasma::Label( 0 );
+    labelTwo->setText( "hello world 2" );
+    h2->addItem(labelTwo);
+    scene.addItem(labelTwo);
 
     view.show();
     return app.exec();
