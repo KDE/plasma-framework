e0f831373c766c8cb58f0698495e12d570ff05c6
Aaron J. Seigo <aseigo@kde.org>
2007-09-14 19:06:18 +0000

    SVN_SILENT: add "or later" clause. cleared with copyright holders, except for rich moore's ui loader which i need to ask him for permission for first.
    svn path=/trunk/KDE/kdebase/workspace/libs/plasma/; revision=712549

--- widgets/radiobutton.h
+++ widgets/radiobutton.h
@@ -3,7 +3,9 @@
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU Library General Public License version 2 as
- *   published by the Free Software Foundation
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+
  *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
