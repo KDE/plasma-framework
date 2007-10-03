5e321b63f564cbe5ea3cdc62ac23e35ea9e55efe
Rafael Fernández López <ereslibre@kde.org>
2007-10-03 00:44:18 +0000

    Update my email address
    svn path=/trunk/KDE/kdebase/workspace/libs/plasma/; revision=720339

--- widgets/radiobutton.cpp
+++ widgets/radiobutton.cpp
@@ -1,5 +1,5 @@
 /*
- *   Copyright 2007 by Rafael Fernández López <ereslibre@gmail.com>
+ *   Copyright 2007 by Rafael Fernández López <ereslibre@kde.org>
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU Library General Public License as
