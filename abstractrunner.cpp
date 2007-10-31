e099821b7163a264dff7c7f89a6e826f6f82247c
Aaron J. Seigo <aseigo@kde.org>
2007-10-31 04:44:09 +0000

    massive change to AbstractRunner API, but now at least it is hopefully future proof and we won't have to change it in BIC ways after 4.0
    introduces two new classes: Plasma::SearchContext and Plasma::SearchAction
    
    benefits include:
     - well, future proofing =)
     - the ability to eventually allow runners that mutate the search
     - multiple exact matches per runner
     - 'executing' of informational runners (useful for, e.g., the calculator)
     - being able to centralize expensive operations such a KUriFilter actions
     - simplifies writing runners *dramatically*
    
    svn path=/trunk/KDE/kdebase/workspace/libs/plasma/; revision=731253

--- abstractrunner.cpp
+++ abstractrunner.cpp
@@ -19,34 +19,249 @@
 
 #include "abstractrunner.h"
 
-#include <QAction>
-#include <KActionCollection>
-#include <KServiceTypeTrader>
 #include <KDebug>
+#include <KMimeType>
+#include <KServiceTypeTrader>
+#include <KUriFilterData>
+
 
 namespace Plasma
 {
 
+class SearchContext::Private
+{
+    public:
+        Private()
+            : type(SearchContext::UnknownType)
+        {
+        }
+
+        void resetState()
+        {
+            qDeleteAll(info);
+            info.clear();
+            qDeleteAll(exact);
+            exact.clear();
+            qDeleteAll(possible);
+            possible.clear();
+            type = SearchContext::UnknownType;
+            term.clear();
+            mimetype.clear();
+        }
+
+        QList<SearchAction *> info;
+        QList<SearchAction *> exact;
+        QList<SearchAction *> possible;
+        QString term;
+        QString mimetype;
+        SearchContext::Type type;
+};
+
+class SearchAction::Private
+{
+    public:
+        Private(SearchContext* s, AbstractRunner *r)
+            : search(s),
+              runner(r),
+              type(SearchAction::ExactMatch),
+              relevance(1)
+        {
+        }
+
+        SearchContext *search;
+        AbstractRunner *runner;
+        SearchAction::Type type;
+        QString mimetype;
+        qreal relevance;
+};
+
 class AbstractRunner::Private
 {
     public:
-        Private( AbstractRunner* runner ) :
-            exactMatch( 0 ),
-            actions( new KActionCollection( runner ) )
-        {
-            delete exactMatch;
-            actions->clear();
-        }
-
-        QAction* exactMatch;
-        KActionCollection* actions;
-        // FIXME: it's a bit lame to keep a copy of the term in each runner
-        QString term;
+        bool hasMatchOptions;
+        bool hasConfig;
 };
 
-AbstractRunner::AbstractRunner( QObject* parent )
-    : QObject( parent ),
-      d( new Private( this ) )
+SearchContext::SearchContext(QObject *parent)
+    : QObject(parent),
+      d(new Private)
+{
+}
+
+SearchContext::~SearchContext()
+{
+    delete d;
+}
+
+void SearchContext::setTerm(const QString &term)
+{
+    d->resetState();
+
+    if (term.isEmpty()) {
+        return;
+    }
+
+    d->term = term;
+    //FIXME: this is insanely slow =/
+    KUriFilterData filter(term);
+    bool filtered = KUriFilter::self()->filterUri(filter);
+
+    if (filtered) {
+        switch (filter.uriType()) {
+            case KUriFilterData::LocalDir:
+                d->type = Directory;
+                d->mimetype = "inode/folder";
+                break;
+            case KUriFilterData::LocalFile: {
+                d->type = File;
+                KMimeType::Ptr mimetype = KMimeType::findByPath(filter.uri().path());
+                if (mimetype) {
+                    d->mimetype = mimetype->name();
+                }
+                break;
+            }
+            case KUriFilterData::NetProtocol:
+                kDebug() << "term is a network protocol?" << term << filter.uriType();
+                d->type = NetworkLocation;
+                break;
+            case KUriFilterData::Executable:
+                d->type = Executable;
+                break;
+            case KUriFilterData::Shell:
+                d->type = ShellCommand;
+                break;
+            case KUriFilterData::Help:
+                d->type = Help;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+QString SearchContext::term() const
+{
+    return d->term;
+}
+
+SearchContext::Type SearchContext::type() const
+{
+    return d->type;
+}
+
+QString SearchContext::mimetype() const
+{
+    return d->mimetype;
+}
+
+SearchAction* SearchContext::addInformationalMatch(AbstractRunner *runner)
+{
+    SearchAction *action = new SearchAction(this, runner);
+    action->setType(SearchAction::InformationalMatch);
+    d->info.append(action);
+    return action;
+}
+
+SearchAction* SearchContext::addExactMatch(AbstractRunner *runner)
+{
+    SearchAction *action = new SearchAction(this, runner);
+    action->setType(SearchAction::ExactMatch);
+    d->exact.append(action);
+    return action;
+}
+
+SearchAction* SearchContext::addPossibleMatch(AbstractRunner *runner)
+{
+    SearchAction *action = new SearchAction(this, runner);
+    action->setType(SearchAction::PossibleMatch);
+    d->possible.append(action);
+    return action;
+}
+
+QList<SearchAction *> SearchContext::informationalMatches() const
+{
+    return d->info;
+}
+
+QList<SearchAction *> SearchContext::exactMatches() const
+{
+    return d->exact;
+}
+
+QList<SearchAction *> SearchContext::possibleMatches() const
+{
+    return d->possible;
+}
+
+
+
+SearchAction::SearchAction(SearchContext *search, AbstractRunner *runner)
+    : QAction(search),
+      d(new Private(search, runner))
+{
+    connect(this, SIGNAL(triggered(bool)), this, SLOT(exec()));
+}
+
+SearchAction::~SearchAction()
+{
+    delete d;
+}
+
+void SearchAction::setType(Type type)
+{
+    d->type = type;
+}
+
+SearchAction::Type SearchAction::type() const
+{
+    return d->type;
+}
+
+void SearchAction::setMimetype(const QString &mimetype)
+{
+    d->mimetype = mimetype;
+}
+
+QString SearchAction::mimetype() const
+{
+    return d->mimetype.isEmpty() ? d->search->mimetype() : d->mimetype;
+}
+
+QString SearchAction::term() const
+{
+    return d->search->term();
+}
+
+void SearchAction::setRelevance(qreal relevance)
+{
+    d->relevance = qMax(0.0, qMin(1.0, relevance));
+}
+
+qreal SearchAction::relevance() const
+{
+    return d->relevance;
+}
+
+AbstractRunner* SearchAction::runner() const
+{
+    return d->runner;
+}
+
+bool SearchAction::operator<(const SearchAction& other) const
+{
+    return d->relevance < other.d->relevance;
+}
+
+void SearchAction::exec()
+{
+    //TODO: this could be dangerous if the runner is deleted behind our backs.
+    d->runner->exec(this);
+}
+
+
+AbstractRunner::AbstractRunner(QObject* parent)
+    : QObject(parent),
+      d(new Private())
 {
 }
 
@@ -55,63 +270,37 @@ AbstractRunner::~AbstractRunner()
     delete d;
 }
 
-bool AbstractRunner::hasOptions()
+bool AbstractRunner::hasMatchOptions()
 {
-    return false;
+    return d->hasMatchOptions;
 }
 
-QWidget* AbstractRunner::options()
+void AbstractRunner::setHasMatchOptions(bool hasMatchOptions)
 {
-    return 0;
+    d->hasMatchOptions = hasMatchOptions;
 }
 
-QAction* AbstractRunner::exactMatch( )
+void AbstractRunner::createMatchOptions(QWidget* parent)
 {
-    return d->exactMatch;
+    Q_UNUSED(parent)
 }
 
-QAction* AbstractRunner::exactMatch( const QString& term )
+bool AbstractRunner::canBeConfigured()
 {
-    delete d->exactMatch;
-    d->term.clear();
-
-    d->exactMatch = accepts( term );
-    if ( d->exactMatch ) {
-        d->term = term;
-        connect( d->exactMatch, SIGNAL( triggered() ),
-                 this, SLOT( runExactMatch() ) );
-    }
-
-    return d->exactMatch;
+    return d->hasConfig;
 }
 
-KActionCollection* AbstractRunner::matches( const QString& term, int max, int offset )
+void AbstractRunner::setCanBeConfigured(bool hasConfig)
 {
-    d->actions->clear();
-    fillMatches( d->actions, term, max, offset );
-    return d->actions;
+    d->hasConfig = hasConfig;
 }
 
-void AbstractRunner::fillMatches( KActionCollection* matches,
-                          const QString& term,
-                          int max, int offset )
+void AbstractRunner::exec(Plasma::SearchAction *action)
 {
-    Q_UNUSED( matches );
-    Q_UNUSED( term );
-    Q_UNUSED( max );
-    Q_UNUSED( offset );
+    Q_UNUSED(action)
 }
 
-void AbstractRunner::runExactMatch()
-{
-    if (!d->exactMatch) {
-        return;
-    }
-
-    exec(d->exactMatch, d->term);
-}
-
-AbstractRunner::List AbstractRunner::loadRunners( QWidget* parent )
+AbstractRunner::List AbstractRunner::loadRunners(QObject* parent)
 {
     List firstRunners;
     List runners;
@@ -122,7 +311,7 @@ AbstractRunner::List AbstractRunner::loadRunners( QWidget* parent )
     foreach (KService::Ptr service, offers) {
         AbstractRunner* runner = service->createInstance<AbstractRunner>(parent, QVariantList(), &error);
         if (runner) {
-            kDebug() << "loaded runner : " << service->name();
+            //kDebug() << "loaded runner : " << service->name();
             QString phase = service->property("X-Plasma-RunnerPhase").toString();
             if (phase == "last") {
                 lastRunners.append(runner);
