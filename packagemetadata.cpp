44f60908b93c06370003c70ba1fbd4e4766e914a
Riccardo Iaconelli <riccardo@kde.org>
2007-11-02 15:30:53 +0000

    SVN_SILENT
    Changing email address from ruphy@fsfe.org to riccardo@kde.org
    
    svn path=/trunk/KDE/kdebase/workspace/libs/plasma/; revision=732026

--- packagemetadata.cpp
+++ packagemetadata.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************
-*   Copyright 2007 by Riccardo Iaconelli  <ruphy@fsfe.org>                *
+*   Copyright 2007 by Riccardo Iaconelli  <riccardo@kde.org>                *
 *                                                                             *
 *   This library is free software; you can redistribute it and/or             *
 *   modify it under the terms of the GNU Library General Public               *
