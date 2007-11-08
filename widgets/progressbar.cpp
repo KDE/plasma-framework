65dadf4f3f0d878a00b16ee83a3529199aa97cb9
Jon de Andres Frias <jondeandres@gmail.com>
2007-11-08 09:52:18 +0000

    Add 'or later' to the license for the progressbar widget in plasmalibs.
    svn path=/trunk/KDE/kdebase/workspace/libs/plasma/; revision=734236

--- widgets/progressbar.cpp
+++ widgets/progressbar.cpp
@@ -2,8 +2,8 @@
  *   Copyright (C) 2007  Jon de Andres Frias <jondeandres@gmail.com>
  *
  *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Library General Public License version 2 as
- *   published by the Free Software Foundation
+ *   it under the terms of the GNU Library General Public License version 2
+ *   or later as published by the Free Software Foundation
  *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -55,7 +55,7 @@ ProgressBar::ProgressBar(Widget *parent)
     init();
 }
 
-ProgressBar::~ProgressBar() 
+ProgressBar::~ProgressBar()
 {
     delete d;
 }
--- widgets/progressbar.h
+++ widgets/progressbar.h
@@ -2,8 +2,8 @@
  *   Copyright (C) 2007  Jon de Andres Frias <jondeandres@gmail.com>
  *
  *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Library General Public License version 2 as
- *   published by the Free Software Foundation
+ *   it under the terms of the GNU Library General Public License version 2 
+ *   or later as published by the Free Software Foundation
  *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
