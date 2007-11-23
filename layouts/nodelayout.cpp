0142f8653c554cdbd48743f01358009ef30275c5
Dirk Mueller <mueller@kde.org>
2007-11-23 00:08:03 +0000

    fix license headers
    svn path=/trunk/KDE/kdebase/workspace/libs/plasma/; revision=740323

--- layouts/nodelayout.cpp
+++ layouts/nodelayout.cpp
@@ -2,7 +2,7 @@
  *   Copyright (C) 2007 Ivan Cukic <ivan.cukic+kde@gmail.com>
  *
  *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Library/Lesser General Public License
+ *   it under the terms of the GNU Lesser General Public License
  *   version 2, or (at your option) any later version, as published by the
  *   Free Software Foundation
  *
@@ -11,7 +11,7 @@
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details
  *
- *   You should have received a copy of the GNU Library/Lesser General Public
+ *   You should have received a copy of the GNU Lesser General Public
  *   License along with this program; if not, write to the
  *   Free Software Foundation, Inc.,
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
