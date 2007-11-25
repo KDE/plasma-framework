bd382fa9833433080a0130685b30211847441502
Tom Albers <toma@kde.org>
2007-11-25 13:28:25 +0000

    Fix wording so it does not result in false positives.
    svn path=/trunk/KDE/kdebase/workspace/libs/plasma/; revision=741335

--- widgets/progressbar.cpp
+++ widgets/progressbar.cpp
@@ -2,8 +2,9 @@
  *   Copyright (C) 2007  Jon de Andres Frias <jondeandres@gmail.com>
  *
  *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Library General Public License version 2
- *   or later as published by the Free Software Foundation
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
  *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
