02023370f2af4fcdb2d032feac4d448a551df10e
Adriaan de Groot <groot@kde.org>
2007-12-10 19:45:45 +0000

    Disambiguate pow()
    svn path=/trunk/KDE/kdebase/workspace/libs/plasma/; revision=746989

--- widgets/signalplotter.cpp
+++ widgets/signalplotter.cpp
@@ -653,7 +653,7 @@ void SignalPlotter::calculateNiceRange()
     // Massage the range so that the grid shows some nice values.
     double step = d->niceVertRange / (d->scaledBy*(d->horizontalLinesCount+1));
     int logdim = (int)floor(log10(step));
-    double dim = pow(10, logdim) / 2;
+    double dim = pow((double)10.0, logdim) / 2;
     int a = (int)ceil(step / dim);
     if (logdim >= 0)
         d->precision = 0;
