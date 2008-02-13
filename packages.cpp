90c26ae4ad497a66982a5b5ea01fb41c529c910c
Aaron J. Seigo <aseigo@kde.org>
2008-02-13 01:39:06 +0000

    update package definition for svg themes
    svn path=/trunk/KDE/kdebase/workspace/libs/plasma/; revision=774332

--- packages.cpp
+++ packages.cpp
@@ -66,12 +66,42 @@ ThemePackageStructure::ThemePackageStructure()
                       i18n("Background image for plasmoids"));
     addFileDefinition("widgets/clock", "widgets/clock.svg",
                       i18n("Analog clock face"));
-    addFileDefinition("widgets/iconbutton", "widgets/iconbutton.svg",
-                      i18n("Back- and foregrounds for clickable icons"));
     addFileDefinition("widgets/panel-background", "widgets/panel-background.svg",
-                      i18n("Default background image for panels"));
+                      i18n("Background image for panels"));
     addFileDefinition("widgets/plot-background", "widgets/plot-background.svg",
                       i18n("Background for graphing widgets"));
+    addFileDefinition("widgets/tooltip", "widgets/tooltip.svg",
+                      i18n("Background image for tooltips"));
+
+    addDirectoryDefinition("opaque/dialogs", "opaque/dialogs/", i18n("Opaque images for dialogs"));
+    addFileDefinition("opaque/dialogs/background", "opaque/dialogs/background.svg",
+                      i18n("Opaque generic dialog background"));
+    addFileDefinition("opaque/dialogs/shutdowndialog", "opaque/dialogs/shutdowndialog.svg",
+                      i18n("Opaque theme for the log out dialog"));
+
+    addDirectoryDefinition("opaque/widgets", "opaque/widgets/", i18n("Opaque images for widgets"));
+    addFileDefinition("opaque/widgets/panel-background", "opaque/widgets/panel-background.svg",
+                      i18n("Opaque background image for panels"));
+    addFileDefinition("opaque/widgets/tooltip", "opaque/widgets/tooltip.svg",
+                      i18n("Opaque background image for tooltips"));
+
+    addDirectoryDefinition("locolor/dialogs", "locolor/dialogs/", i18n("Low color images for dialogs"));
+    addFileDefinition("locolor/dialogs/background", "locolor/dialogs/background.svg",
+                      i18n("Low color generic dialog background"));
+    addFileDefinition("locolor/dialogs/shutdowndialog", "locolor/dialogs/shutdowndialog.svg",
+                      i18n("Low color theme for the log out dialog"));
+
+    addDirectoryDefinition("locolor/widgets", "locolor/widgets/", i18n("Images for widgets"));
+    addFileDefinition("locolor/widgets/background", "locolor/widgets/background.svg",
+                      i18n("Low color background image for plasmoids"));
+    addFileDefinition("locolor/widgets/clock", "locolor/widgets/clock.svg",
+                      i18n("Low color analog clock face"));
+    addFileDefinition("locolor/widgets/panel-background", "locolor/widgets/panel-background.svg",
+                      i18n("Low color background image for panels"));
+    addFileDefinition("locolor/widgets/plot-background", "locolor/widgets/plot-background.svg",
+                      i18n("Low color background for graphing widgets"));
+    addFileDefinition("locolor/widgets/tooltip", "locolor/widgets/tooltip.svg",
+                      i18n("Low color background image for tooltips"));
 
     addFileDefinition("colors", "colors", i18n("KColorScheme configuration file"));
 
