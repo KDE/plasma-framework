5ea3cb88e1c5f56a98dc8c555a9f5c1bd3cba76b
Aaron J. Seigo <aseigo@kde.org>
2008-03-06 20:24:59 +0000

    give us a bit more of a border so faster mouse movements work too, without ruining the whole effect
    svn path=/trunk/KDE/kdebase/workspace/libs/plasma/; revision=783029

--- desktoptoolbox.cpp
+++ desktoptoolbox.cpp
@@ -161,7 +161,7 @@ void DesktopToolbox::hoverEnterEvent(QGraphicsSceneHoverEvent *event)
     }
 
     QPainterPath path;
-    int size = m_size + (int)m_animFrame - 5;
+    int size = m_size + (int)m_animFrame - 15;
     path.moveTo(m_size*2, 0);
     path.arcTo(QRectF(m_size * 2 - size, -size, size*2, size*2), 180, 90);
     path.lineTo(m_size*2, 0);
