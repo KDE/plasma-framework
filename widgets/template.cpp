c0244c2c682741c0c8251c4d4966bac71a60fff8
Aaron J. Seigo <aseigo@kde.org>
2008-05-18 22:27:29 +0000

    a little system for making widgets quickly
    svn path=/trunk/KDE/kdebase/workspace/libs/plasma/; revision=809454

---
+++ widgets/template.cpp
@@ -0,0 +1,154 @@
+/*
+ *   Copyright 2008 Aaron Seigo <aseigo@kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "<name>.h"
+
+#include <<Native>>
+#include <QPainter>
+
+#include <KMimeType>
+
+#include "theme.h"
+#include "svg.h"
+
+namespace Plasma
+{
+
+class <Name>::Private
+{
+public:
+    Private()
+        : svg(0)
+    {
+    }
+
+    ~Private()
+    {
+        delete svg;
+    }
+
+    void setPixmap(<Name> *q)
+    {
+        if (imagePath.isEmpty()) {
+            return;
+        }
+
+        KMimeType::Ptr mime = KMimeType::findByPath(absImagePath);
+        QPixmap pm(q->size().toSize());
+
+        if (mime->is("image/svg+xml")) {
+            svg = new Svg();
+            QPainter p(&pm);
+            svg->paint(&p, pm.rect());
+        } else {
+            pm = QPixmap(absImagePath);
+        }
+
+        //TODO: load image into widget
+        //static_cast<<Native>*>(widget())->setPixmap();
+        //static_cast<<Native>*>(widget())->setIcon(QIcon();
+    }
+
+    QString imagePath;
+    QString absImagePath;
+    Svg *svg;
+};
+
+<Name>::<Name>(QGraphicsWidget *parent)
+    : QGraphicsProxyWidget(parent),
+      d(new Private)
+{
+    <Native>* native = new <Native>;
+    //TODO: forward signals
+    setWidget(native);
+}
+
+<Name>::~<Name>()
+{
+    delete d;
+}
+
+void <Name>::setText(const QString &text)
+{
+    static_cast<<Native>*>(widget())->setText(text);
+}
+
+QString <Name>::text() const
+{
+    return static_cast<<Native>*>(widget())->text();
+}
+
+void <Name>::setImage(const QString &path)
+{
+    if (d->imagePath == path) {
+        return;
+    }
+
+    delete d->svg;
+    d->svg = 0;
+    d->imagePath = path;
+
+    bool absolutePath = !path.isEmpty() && 
+                        #ifdef Q_WS_WIN
+                            !QDir::isRelativePath(path)
+                        #else
+                            path[0] == '/'
+                        #endif
+        ;
+
+    if (absolutePath) {
+        d->absImagePath = path;
+    } else {
+        //TODO: package support
+        d->absImagePath = Theme::defaultTheme()->imagePath(path);
+    }
+
+    d->setPixmap(this);
+}
+
+QString <Name>::image() const
+{
+    return d->imagePath;
+}
+
+void <Name>::setStylesheet(const QString &stylesheet)
+{
+    widget()->setStyleSheet(stylesheet);
+}
+
+QString <Name>::stylesheet()
+{
+    return widget()->styleSheet();
+}
+
+<Native>* <Name>::nativeWidget() const
+{
+    return static_cast<<Native>*>(widget());
+}
+
+void <Name>::resizeEvent(QGraphicsSceneResizeEvent *event)
+{
+    Q_UNUSED(event)
+    d->setPixmap(this);
+}
+
+} // namespace Plasma
+
+#include <<name>.moc>
+
