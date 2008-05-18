8c21c9f2cad743381399746afac9f2854da8aae0
Sebastian Sauer <mail@dipe.org>
2008-05-18 17:16:10 +0000

    did the todo. At Package::registerPackage() used the PackageMetadata's serviceType if defined else use 'Plasma/Applet,Plasma/Containment' like before.
    svn path=/trunk/KDE/kdebase/workspace/libs/plasma/; revision=809278

--- package.cpp
+++ package.cpp
@@ -339,9 +339,8 @@ bool Package::registerPackage(const PackageMetadata &data, const QString &iconPa
     KDesktopFile config(service);
     KConfigGroup cg = config.desktopGroup();
     cg.writeEntry("Type", "Service");
-    //TODO do we really like to just install all packages as applet/containment? Probably
-    //it would make sense to let the packages themself decide what they are.
-    cg.writeEntry("X-KDE-ServiceTypes", "Plasma/Applet,Plasma/Containment");
+    const QString serviceTypes = data.serviceType().isNull() ? "Plasma/Applet,Plasma/Containment" : data.serviceType();
+    cg.writeEntry("X-KDE-ServiceTypes", serviceTypes);
     cg.writeEntry("X-KDE-PluginInfo-EnabledByDefault", true);
 
     QFile icon(iconPath);
