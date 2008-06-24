8a974f0f468d498fc645f2fa124f6222cb9d1dad
Aaron J. Seigo <aseigo@kde.org>
2008-06-24 03:16:33 +0000

    small changes that make the code more readable
    svn path=/trunk/KDE/kdebase/workspace/libs/plasma/; revision=823769

--- containment.cpp
+++ containment.cpp
@@ -1101,16 +1101,14 @@ void Containment::Private::positionToolBox()
             }
         }
     } else {
-        QRectF r;
         QDesktopWidget *desktop = QApplication::desktop();
-        r = desktop->availableGeometry(screen);
+        QRectF r = desktop->availableGeometry(screen);
         if (q->view() && !q->view()->transform().isScaling()) {
             toolBox->setPos(r.topRight());
         } else {
             toolBox->setPos(q->mapFromScene(QPointF(q->geometry().topRight())));
         }
     }
-
 }
 
 void Containment::Private::triggerShowAddWidgets()
