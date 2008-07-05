0d9ab9ab383f1f02bcdedf49c4ebf99b3939abce
Aaron J. Seigo <aseigo@kde.org>
2008-07-05 05:13:00 +0000

    it's *always* Type=Service
    svn path=/trunk/KDE/kdebase/workspace/libs/plasma/; revision=828239

--- package.cpp
+++ package.cpp
@@ -344,8 +344,7 @@ bool Package::registerPackage(const PackageMetadata &data, const QString &iconPa
 
     KDesktopFile config(service);
     KConfigGroup cg = config.desktopGroup();
-    const QString type = data.type().isNull() ? "Service" : data.type();
-    cg.writeEntry("Type", type);
+    cg.writeEntry("Type", "Service");
     const QString serviceTypes = data.serviceType().isNull() ? "Plasma/Applet,Plasma/Containment" : data.serviceType();
     cg.writeEntry("X-KDE-ServiceTypes", serviceTypes);
     cg.writeEntry("X-KDE-PluginInfo-EnabledByDefault", true);
