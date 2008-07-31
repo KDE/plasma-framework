4335bba59cee2f6db0378c1223744b8701387e6d
Alex Merry <kde@randomguy3.me.uk>
2008-07-31 14:03:22 +0000

    Remove two FIXMEs: apparently this is needed after all (it screws up the device notifier at least without it).
    SVN_SILENT
    
    
    svn path=/trunk/KDE/kdebase/workspace/libs/plasma/; revision=840155

--- widgets/icon.cpp
+++ widgets/icon.cpp
@@ -975,8 +975,6 @@ void Icon::setText(const QString& text)
 {
     d->text = text;
     // cause a relayout
-    // FIXME: is invalidating d->currentSize really necessary?
-    // FIXME: is d->currentSize really necessary at all?
     d->currentSize = QSizeF(-1, -1);
     resize(sizeFromIconSize(d->iconSize.width()));
 }
@@ -990,8 +988,6 @@ void Icon::setInfoText(const QString& text)
 {
     d->infoText = text;
     // cause a relayout
-    // FIXME: is invalidating d->currentSize really necessary?
-    // FIXME: is d->currentSize really necessary at all?
     d->currentSize = QSizeF(-1, -1);
     resize(sizeFromIconSize(d->iconSize.width()));
 }
