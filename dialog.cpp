90f5feb1a6af1650699748cac94047a2ef6f4b2a
Alex Merry <kde@randomguy3.me.uk>
2008-09-07 21:33:02 +0000

    Relicense files.  See plasma-devel@kde.org mailing list archives for permissions.
    svn path=/trunk/KDE/kdebase/workspace/libs/plasma/; revision=858364

--- dialog.cpp
+++ dialog.cpp
@@ -1,24 +1,24 @@
-/***************************************************************************
- *   Copyright (C) 2008 by Alessandro Diaferia <alediaferia@gmail.com>     *
- *   Copyright (C) 2007 by Alexis Ménard <darktears31@gmail.com>           *
- *   Copyright (C) 2007 Sebastian Kuegler <sebas@kde.org>                  *
- *   Copyright (C) 2006 Aaron Seigo <aseigo@kde.org>		   	   *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
- ***************************************************************************/
+/*
+ * Copyright 2008 by Alessandro Diaferia <alediaferia@gmail.com>
+ * Copyright 2007 by Alexis Ménard <darktears31@gmail.com>
+ * Copyright 2007 Sebastian Kuegler <sebas@kde.org>
+ * Copyright 2006 Aaron Seigo <aseigo@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
 
 #include "dialog.h"
 
