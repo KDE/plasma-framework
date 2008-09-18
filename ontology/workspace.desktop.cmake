66ddad6aacfff13eeef0e3379891044c8facc8bd
Aaron J. Seigo <aseigo@kde.org>
2008-09-18 05:08:01 +0000

    being replaced by the new ontology from the nepomukians
    svn path=/trunk/KDE/kdebase/workspace/libs/plasma/; revision=862108

--- ontology/workspace.desktop.cmake
+++
@@ -1,7 +0,0 @@
-[Desktop Entry]
-Version=1.0
-Name=Workspace Context Ontology
-URL=http://plasma.kde.org/ontologies/workspacecontext#
-Path=${DATA_INSTALL_DIR}/nepomuk/ontologies/workspace.trig
-MimeType=application/x-trig
-Type=Data
