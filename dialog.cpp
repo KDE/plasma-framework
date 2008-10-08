1a196323d20df20693c0f14470479ad5341b3c6a
Marco Martin <notmart@gmail.com>
2008-10-08 16:47:29 +0000

    correctly resize the graphicswidget when the dialog corners are dragged
    svn path=/trunk/KDE/kdebase/workspace/libs/plasma/; revision=869267

--- dialog.cpp
+++ dialog.cpp
@@ -283,7 +283,16 @@ void Dialog::resizeEvent(QResizeEvent *e)
     setMask(d->background->mask());
 
     if (d->resizeStartCorner != Dialog::NoCorner && d->view && d->widget) {
-        d->widget->resize(d->view->size());
+        d->widget->setPreferredSize(d->view->size());
+
+        QGraphicsLayoutItem *layout = d->widget->parentLayoutItem();
+        QGraphicsWidget *parentWidget = d->widget->parentWidget();
+
+        if (layout && parentWidget) {
+            layout->updateGeometry();
+            parentWidget->resize(layout->preferredSize());
+        }
+
         d->view->setSceneRect(d->widget->mapToScene(d->widget->boundingRect()).boundingRect());
         d->view->centerOn(d->widget);
     }
