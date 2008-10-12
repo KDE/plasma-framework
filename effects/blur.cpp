4647b01da4b969c712234ebe7795d647767535b7
Allen Winter <winter@kde.org>
2008-10-12 01:46:21 +0000

    coding style fixes. that's enough for now.
    svn path=/trunk/KDE/kdebase/workspace/libs/plasma/; revision=870214

--- effects/blur.cpp
+++ effects/blur.cpp
@@ -27,10 +27,10 @@ template<int aprec, int zprec>
 static inline void blurinner(unsigned char *bptr, int &zR, int &zG, int &zB, int &zA, int alpha);
 
 template<int aprec,int zprec>
-static inline void blurrow( QImage & im, int line, int alpha);
+static inline void blurrow(QImage &im, int line, int alpha);
 
 template<int aprec, int zprec>
-static inline void blurcol( QImage & im, int col, int alpha);
+static inline void blurcol(QImage &im, int col, int alpha);
 
 /*
 *  expblur(QImage &img, int radius)
@@ -41,32 +41,31 @@ static inline void blurcol( QImage & im, int col, int alpha);
 *  Blurs with two sided exponential impulse
 *  response.
 *
-*  aprec = precision of alpha parameter 
+*  aprec = precision of alpha parameter
 *  in fixed-point format 0.aprec
 *
 *  zprec = precision of state parameters
 *  zR,zG,zB and zA in fp format 8.zprec
 */
 template<int aprec,int zprec>
-void expblur( QImage &img, int radius )
+void expblur(QImage &img, int radius)
 {
-  if(radius<1)
+    if(radius < 1) {
     return;
-
-  /* Calculate the alpha such that 90% of 
-     the kernel is within the radius.
-     (Kernel extends to infinity) 
-  */
-  int alpha = (int)((1<<aprec)*(1.0f-std::exp(-2.3f/(radius+1.f))));
-
-  for(int row=0;row<img.height();row++)
-  {
-    blurrow<aprec,zprec>(img,row,alpha);
   }
 
-  for(int col=0;col<img.width();col++)
-  {
-    blurcol<aprec,zprec>(img,col,alpha);
+  /* Calculate the alpha such that 90% of
+     the kernel is within the radius.
+     (Kernel extends to infinity)
+  */
+  int alpha = (int)((1 << aprec) * (1.0f - std::exp(-2.3f / (radius + 1.f))));
+
+  for (int row=0; row<img.height(); row++) {
+    blurrow<aprec,zprec>(img, row, alpha);
+  }
+
+  for (int col=0; col<img.width(); col++) {
+    blurcol<aprec,zprec>(img, col, alpha);
   }
   return;
 }
@@ -74,78 +73,75 @@ void expblur( QImage &img, int radius )
 template<int aprec, int zprec>
 static inline void blurinner(unsigned char *bptr, int &zR, int &zG, int &zB, int &zA, int alpha)
 {
-  int R,G,B,A;
+  int R, G, B, A;
   R = *bptr;
-  G = *(bptr+1);
-  B = *(bptr+2);
-  A = *(bptr+3);
+  G = *(bptr + 1);
+  B = *(bptr + 2);
+  A = *(bptr + 3);
 
-  zR += (alpha * ((R<<zprec)-zR))>>aprec;
-  zG += (alpha * ((G<<zprec)-zG))>>aprec;
-  zB += (alpha * ((B<<zprec)-zB))>>aprec;
-  zA += (alpha * ((A<<zprec)-zA))>>aprec;
+  zR += (alpha * ((R << zprec) - zR)) >> aprec;
+  zG += (alpha * ((G << zprec) - zG)) >> aprec;
+  zB += (alpha * ((B << zprec) - zB)) >> aprec;
+  zA += (alpha * ((A << zprec) - zA)) >> aprec;
 
-  *bptr =     zR>>zprec;
-  *(bptr+1) = zG>>zprec;
-  *(bptr+2) = zB>>zprec;
-  *(bptr+3) = zA>>zprec;
+  *bptr =     zR >> zprec;
+  *(bptr+1) = zG >> zprec;
+  *(bptr+2) = zB >> zprec;
+  *(bptr+3) = zA >> zprec;
 }
 
 template<int aprec,int zprec>
-static inline void blurrow( QImage & im, int line, int alpha)
+static inline void blurrow(QImage &im, int line, int alpha)
 {
-  int zR,zG,zB,zA;
+  int zR, zG, zB, zA;
 
   QRgb *ptr = (QRgb *)im.scanLine(line);
 
-  zR = *((unsigned char *)ptr    )<<zprec;
-  zG = *((unsigned char *)ptr + 1)<<zprec;
-  zB = *((unsigned char *)ptr + 2)<<zprec;
-  zA = *((unsigned char *)ptr + 3)<<zprec;
+  zR = *((unsigned char *)ptr    ) << zprec;
+  zG = *((unsigned char *)ptr + 1) << zprec;
+  zB = *((unsigned char *)ptr + 2) << zprec;
+  zA = *((unsigned char *)ptr + 3) << zprec;
 
-  for(int index=1; index<im.width(); index++)
-  {
-    blurinner<aprec,zprec>((unsigned char *)&ptr[index],zR,zG,zB,zA,alpha);
+  for (int index=1; index<im.width(); index++) {
+      blurinner<aprec,zprec>((unsigned char *)&ptr[index],zR,zG,zB,zA,alpha);
   }
-  for(int index=im.width()-2; index>=0; index--)
-  {
-    blurinner<aprec,zprec>((unsigned char *)&ptr[index],zR,zG,zB,zA,alpha);
+  for (int index=im.width()-2; index>=0; index--) {
+      blurinner<aprec,zprec>((unsigned char *)&ptr[index],zR,zG,zB,zA,alpha);
   }
- 
-
 }
 
 template<int aprec, int zprec>
-static inline void blurcol( QImage & im, int col, int alpha)
+static inline void blurcol(QImage &im, int col, int alpha)
 {
-  int zR,zG,zB,zA;
+  int zR, zG, zB, zA;
 
   QRgb *ptr = (QRgb *)im.bits();
-  ptr+=col;
+  ptr += col;
 
-  zR = *((unsigned char *)ptr    )<<zprec;
-  zG = *((unsigned char *)ptr + 1)<<zprec;
-  zB = *((unsigned char *)ptr + 2)<<zprec;
-  zA = *((unsigned char *)ptr + 3)<<zprec;
+  zR = *((unsigned char *)ptr    ) << zprec;
+  zG = *((unsigned char *)ptr + 1) << zprec;
+  zB = *((unsigned char *)ptr + 2) << zprec;
+  zA = *((unsigned char *)ptr + 3) << zprec;
 
-  for(int index=im.width(); index<(im.height()-1)*im.width(); index+=im.width())
-  {
-    blurinner<aprec,zprec>((unsigned char *)&ptr[index],zR,zG,zB,zA,alpha);
+  for (int index=im.width(); index<(im.height()-1)*im.width(); index+=im.width()) {
+      blurinner<aprec,zprec>((unsigned char *)&ptr[index], zR, zG, zB, zA, alpha);
   }
 
-  for(int index=(im.height()-2)*im.width(); index>=0; index-=im.width())
-  {
-    blurinner<aprec,zprec>((unsigned char *)&ptr[index],zR,zG,zB,zA,alpha);
+  for (int index=(im.height()-2)*im.width(); index>=0; index-=im.width()) {
+      blurinner<aprec,zprec>((unsigned char *)&ptr[index], zR, zG, zB, zA, alpha);
   }
-
 }
 
 template<class T>
-inline const T& qClamp(const T &x, const T &low, const T &high)
+inline const T &qClamp(const T &x, const T &low, const T &high)
 {
-    if      (x <  low) return low;
-    else if (x > high) return high;
-    else               return x;
+    if (x <  low) {
+        return low;
+    } else if (x > high) {
+        return high;
+    } else {
+        return x;
+    }
 }
 
 #endif
