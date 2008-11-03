44f44ebfeaed26bdfce0d560f259801ab900976b
Aaron J. Seigo <aseigo@kde.org>
2008-11-03 23:07:38 +0000

    ok, let's put this back, then try one more time and get it right this time. gh
    svn path=/trunk/KDE/kdelibs/; revision=879758

--- kdm/Messages.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-$XGETTEXT kgreet_classic.cpp -o $podir/kgreet_classic.pot
-$XGETTEXT kgreet_winbind.cpp -o $podir/kgreet_winbind.pot
-$XGETTEXT kgreet_generic.cpp -o $podir/kgreet_generic.pot
--- taskmanager/Messages.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/env bash
-$XGETTEXT *.cpp */*.cpp *.h -o $podir/libtaskmanager.pot
