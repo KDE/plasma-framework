ff775cfaf0c6dec9d5a5ef9413dbd5098d64c25f
Marco Martin <notmart@gmail.com>
2008-11-11 12:52:25 +0000

    SVN_SILENT: whoops slipped a copypasted copyright in the header :)
    svn path=/trunk/KDE/kdelibs/; revision=882819

--- widgets/toolbutton.h
+++ widgets/toolbutton.h
@@ -1,5 +1,5 @@
 /*
- *   Copyright 2008 Aaron Seigo <aseigo@kde.org>
+ *   Copyright 2008 Marco Martin <notmart@gmail.com>
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU Library General Public License as
