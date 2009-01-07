bd9131acfa467ff4aa6cc6df55f1a602efa91158
Aaron J. Seigo <aseigo@kde.org>
2009-01-07 20:28:10 +0000

    actually test writing to the config
    svn path=/trunk/KDE/kdebase/workspace/plasma/scriptengines/javascript/; revision=907290

--- scriptengines/javascript/tests/javascript-config-test/contents/code/main.js
+++ scriptengines/javascript/tests/javascript-config-test/contents/code/main.js
@@ -2,4 +2,6 @@ print(plasmoid.readConfig("Test"));
 plasmoid.activeConfig = "secondary";
 print(plasmoid.activeConfig);
 print(plasmoid.readConfig("Test"));
+plasmoid.writeConfig("Test", "You should see this!");
+print(plasmoid.readConfig("Test"));
 
