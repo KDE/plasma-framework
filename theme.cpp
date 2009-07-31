9e6bdba5929856b5ade22ae38d2fcc706d8d5a14
Marco Martin <notmart@gmail.com>
2009-07-31 18:59:45 +0000

    backport disk cache sync in releaseRectsCache
    svn path=/branches/KDE/4.3/kdelibs/; revision=1005305

--- theme.cpp
+++ theme.cpp
@@ -776,6 +776,8 @@ void Theme::releaseRectsCache(const QString &image)
 {
     QHash<QString, QSet<QString> >::iterator it = d->invalidElements.find(image);
     if (it != d->invalidElements.end()) {
+        KConfigGroup imageGroup(d->svgElementsCache, it.key());
+        imageGroup.writeEntry("invalidElements", it.value().toList());
         d->invalidElements.erase(it);
     }
 }
