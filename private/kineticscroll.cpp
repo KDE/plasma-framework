ac7e75f46062f586b82abbadef00b309f1673aff
Marco Martin <notmart@gmail.com>
2009-12-05 22:18:15 +0000

    unfortunately this has to be disabled since blocks clicks on webview :/
    svn path=/trunk/KDE/kdelibs/; revision=1059153

--- private/kineticscroll.cpp
+++ private/kineticscroll.cpp
@@ -234,12 +234,6 @@ void KineticScrolling::mousePressEvent(QGraphicsSceneMouseEvent *event)
     d->syncViewportRect();
     d->cposition = -d->parent->property("scrollPosition").value<QPointF>();
 
-    if (event && d->cposition == QPointF(0, 0) &&
-            d->viewportGeometry.width() + 2 >= d->contentsSize.width() &&
-            d->viewportGeometry.height() + 2 >= d->contentsSize.height()) {
-        event->ignore( );
-    }
-
     d->count();
     d->kinMovement = QPointF(0,0);
     d->scrollAnimation->setEasingCurve(QEasingCurve::OutCirc);
