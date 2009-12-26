bf438dcf98ea8ae873ce0a99ac49cabbe6b35f1d
Marco Martin <notmart@gmail.com>
2009-12-26 10:37:47 +0000

    be LGPL2+
    svn path=/trunk/KDE/kdelibs/; revision=1066185

--- widgets/itembackground.cpp
+++ widgets/itembackground.cpp
@@ -3,8 +3,8 @@
  *   Copyright 2009 by Marco Martin <notmart@gmail.com>                    *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2, or       *
  *   (at your option) any later version.                                   *
  *                                                                         *
  *   This program is distributed in the hope that it will be useful,       *
