c1e947a76848107bc0d355a5b1014042ab383eab
Marco Martin <notmart@gmail.com>
2010-01-07 21:58:51 +0000

    when the icon is horizontal and there is no text take the min between width and height as icon size. among other things fixes the unhide arrow of the systray
    svn path=/trunk/KDE/kdelibs/; revision=1071365

--- widgets/iconwidget.cpp
+++ widgets/iconwidget.cpp
@@ -546,9 +546,12 @@ void IconWidgetPrivate::layoutIcons(const QStyleOptionGraphicsItem *option)
         //if there is text resize the icon in order to make room for the text
         if (text.isEmpty() && infoText.isEmpty()) {
             // with no text, we just take up the whole geometry
-            iconWidth = currentSize.height() -
-                        horizontalMargin[IconWidgetPrivate::IconMargin].left -
-                        horizontalMargin[IconWidgetPrivate::IconMargin].right;
+            iconWidth = qMin(currentSize.height() -
+                            horizontalMargin[IconWidgetPrivate::IconMargin].left -
+                            horizontalMargin[IconWidgetPrivate::IconMargin].right,
+                             currentSize.width() -
+                            horizontalMargin[IconWidgetPrivate::IconMargin].left -
+                            horizontalMargin[IconWidgetPrivate::IconMargin].right);
         } else {
             iconWidth = currentSize.height() -
                         verticalMargin[IconWidgetPrivate::IconMargin].top -
