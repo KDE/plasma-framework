fa74621c218252e8fe33ac7b7bd990466f737d27
Rob Scheepmaker <r.scheepmaker@student.utwente.nl>
2010-01-18 18:24:57 +0000

    relicense to LGPL v2
    svn path=/trunk/KDE/kdelibs/; revision=1076714

--- private/joliemessagehelper_p.h
+++ private/joliemessagehelper_p.h
@@ -1,10 +1,9 @@
 /*
- *   Copyright 2009 Rob Scheepmaker <r.scheepmaker@student.utwente.org>
+ *   Copyright © 2009 Rob Scheepmaker <r.scheepmaker@student.utwente.nl>
  *
  *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as
- *   published by the Free Software Foundation; either version 2,
- *   or (at your option) any later version.
+ *   it under the terms of the GNU Library General Public License version 2 as
+ *   published by the Free Software Foundation
  *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
