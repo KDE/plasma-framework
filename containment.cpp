acc4b9ce1faed6cf7f063715d1c43e11b3853b91
Aaron J. Seigo <aseigo@kde.org>
2010-02-22 16:32:05 +0000

    it's ok if the X-KDE-ParentApp line exists but is empty
    svn path=/trunk/KDE/kdelibs/; revision=1094363

--- containment.cpp
+++ containment.cpp
@@ -1081,7 +1081,7 @@ KPluginInfo::List Containment::listContainmentsOfType(const QString &type,
     QString constraint;
 
     if (parentApp.isEmpty()) {
-        constraint.append("not exist [X-KDE-ParentApp]");
+        constraint.append("(not exist [X-KDE-ParentApp] or [X-KDE-ParentApp] == '')");
     } else {
         constraint.append("[X-KDE-ParentApp] == '").append(parentApp).append("'");
     }
