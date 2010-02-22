c8cd5ca2587b01396a4093a3d76735dec149ad33
Davide Bettio <davide.bettio@kdemail.net>
2010-02-22 23:24:05 +0000

    Sorry, I commited the wrong file.
    svn path=/trunk/KDE/kdelibs/; revision=1094591

--- private/applethandle.cpp
+++ private/applethandle.cpp
@@ -731,7 +731,6 @@ void AppletHandle::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
 bool AppletHandle::sceneEvent(QEvent *event)
 {
     switch (event->type()) {
-#if 0
     case QEvent::TouchEnd: {
         QTransform t = m_applet->transform();
         QRectF geom = m_applet->geometry();
@@ -779,11 +778,6 @@ bool AppletHandle::sceneEvent(QEvent *event)
 
         }
         return true;
-#endif
-    case QEvent::Gesture
-    {
-      QGesture *gesture = static_cast<QGestureEvent*>(event);
-            return true;
     }
     default:
         break;
