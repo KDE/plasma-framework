ea1e8d4b93b8423d967719189f915c7333c0ebc4
Marco Martin <notmart@gmail.com>
2010-04-09 12:26:01 +0000

    move the dialog on resize only it it waasn't the user resizing it manually
    svn path=/trunk/KDE/kdelibs/; revision=1112886

--- dialog.cpp
+++ dialog.cpp
@@ -568,16 +568,18 @@ void Dialog::resizeEvent(QResizeEvent *event)
         d->view->centerOn(graphicsWidget);
     }
 
-    Plasma::Applet *applet = d->applet();
-    if (applet && !event->oldSize().isEmpty()) {
-        Plasma::Corona *corona = qobject_cast<Plasma::Corona *>(applet->scene());
-        if (corona) {
-            QSize deltaSize(event->size() - event->oldSize());
+    if (d->resizeStartCorner == -1) {
+        Plasma::Applet *applet = d->applet();
+        if (applet && !event->oldSize().isEmpty()) {
+            Plasma::Corona *corona = qobject_cast<Plasma::Corona *>(applet->scene());
+            if (corona) {
+                QSize deltaSize(event->size() - event->oldSize());
 
-            if (applet->location() == Plasma::BottomEdge) {
-                move(pos() - QPoint(0, deltaSize.height()));
-            } else if (applet->location() == Plasma::RightEdge) {
-                move(pos() - QPoint(deltaSize.width(), 0));
+                if (applet->location() == Plasma::BottomEdge) {
+                    move(pos() - QPoint(0, deltaSize.height()));
+                } else if (applet->location() == Plasma::RightEdge) {
+                    move(pos() - QPoint(deltaSize.width(), 0));
+                }
             }
         }
     }
