d38dc618197e5d04ee0785a1db6f7e14e2b9d270
Aaron J. Seigo <aseigo@kde.org>
2010-04-26 20:46:36 +0000

    clean up the copyright headers
    svn path=/trunk/KDE/kdelibs/; revision=1119236

--- animations/engine.h
+++ animations/engine.h
@@ -1,32 +1,20 @@
-/***********************************************************************/
-/* engine.h 							       */
-/* 								       */
-/* Copyright (C)  2010  Adenilson Cavalcanti <cavalcantii@gmail.com>   */
-/* 								       */
-/* This library is free software; you can redistribute it and/or       */
-/* modify it under the terms of the GNU Lesser General Public	       */
-/* License as published by the Free Software Foundation; either	       */
-/* version 2.1 of the License, or (at your option) any later version.  */
-/*   								       */
-/* This library is distributed in the hope that it will be useful,     */
-/* but WITHOUT ANY WARRANTY; without even the implied warranty of      */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU   */
-/* Lesser General Public License for more details.		       */
-/*  								       */
-/* You should have received a copy of the GNU Lesser General Public    */
-/* License along with this library; if not, write to the Free Software */
-/* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA       */
-/* 02110-1301  USA						       */
-/***********************************************************************/
-
-/**
- * @file   engine.h
- * @author Adenilson Cavalcanti
- * @date   Wed Feb 24 21:49:45 2010
+/*
+ * Copyright (C)  2010  Adenilson Cavalcanti <cavalcantii@gmail.com>
  *
- * @brief
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
  *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
  *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 #ifndef __ENGINE__
