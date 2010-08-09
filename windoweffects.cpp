0fea8dc29f248ee493ddc949e5492e55a6072431
Marco Martin <notmart@gmail.com>
2010-08-09 14:51:34 +0000

    Plasma::Theme::windowTranslucencyEnabled() caches the selection owner, should be faster than KWindowsystem::compositingActive() that doesn't
    svn path=/trunk/KDE/kdelibs/; revision=1161045

--- windoweffects.cpp
+++ windoweffects.cpp
@@ -22,6 +22,8 @@
 
 #include <kwindowsystem.h>
 
+#include "theme.h"
+
 #ifdef Q_WS_X11
     #include <X11/Xlib.h>
     #include <X11/Xatom.h>
@@ -41,7 +43,7 @@ namespace WindowEffects
 //FIXME: check if this works for any atom?
 bool isEffectAvailable(Effect effect)
 {
-    if (!KWindowSystem::compositingActive()) {
+    if (!Plasma::Theme::defaultTheme()->windowTranslucencyEnabled()) {
         return false;
     }
 #ifdef Q_WS_X11
