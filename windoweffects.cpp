c462eabf81980fa8b445f3489e8e9b90c55aeef3
Aaron Seigo <aseigo@kde.org>
2011-05-28 00:38:34 +0200

    getting the compiler to stfu

--- windoweffects.cpp
+++ windoweffects.cpp
@@ -30,8 +30,8 @@
     #include <X11/Xutil.h>
     #include <QX11Info>
 
-    #define DASHBOARD_WIN_NAME "dashboard"
-    #define DASHBOARD_WIN_CLASS "dashboard"
+    static const char *DASHBOARD_WIN_NAME = "dashboard";
+    static const char *DASHBOARD_WIN_CLASS = "dashboard";
 #endif
 
 namespace Plasma
@@ -335,8 +335,8 @@ void markAsDashboard(WId window)
 {
 #ifdef Q_WS_X11
     XClassHint classHint;
-    classHint.res_name = DASHBOARD_WIN_NAME;
-    classHint.res_class = DASHBOARD_WIN_CLASS;
+    classHint.res_name = const_cast<char *>(DASHBOARD_WIN_NAME);
+    classHint.res_class = const_cast<char *>(DASHBOARD_WIN_CLASS);
     XSetClassHint(QX11Info::display(), window, &classHint);
 #endif
 }
