201baafc1c7a0480e993ca771f96953c76fb854c
Aaron Seigo <aseigo@kde.org>
2011-07-13 13:14:40 +0200

    mainitem can be null according to the if() below, so respect that

--- declarativeimports/core/dialog.cpp
+++ declarativeimports/core/dialog.cpp
@@ -106,9 +106,11 @@ void DialogProxy::setMainItem(QGraphicsObject *mainItem)
 {
     if (m_mainItem.data() != mainItem) {
         if (m_mainItem) {
-            m_mainItem.data()->setParent(mainItem->parent());
+            m_mainItem.data()->setParent(mainItem ? mainItem->parent() : 0);
         }
+
         m_mainItem = mainItem;
+
         if (mainItem) {
             mainItem->setParentItem(0);
             mainItem->setParent(this);
