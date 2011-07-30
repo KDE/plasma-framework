25869b4e9db9a77f6b5e6e57a43af826b533a372
Christoph Feck <christoph@maxiom.de>
2011-07-30 21:18:38 +0200

    Fix accessing pixmapCache after it is deleted
    BUG: 275570
    FIXED-IN: 4.7.1
    (cherry picked from commit f5b095fdaaae164ef04dd46dfb4fb97a8aa87768)

--- theme.cpp
+++ theme.cpp
@@ -310,10 +310,12 @@ void ThemePrivate::discardCache(CacheTypes caches)
 
 void ThemePrivate::scheduledCacheUpdate()
 {
-    QHashIterator<QString, QPixmap> it(pixmapsToCache);
-    while (it.hasNext()) {
-        it.next();
-        pixmapCache->insertPixmap(idsToCache[it.key()], it.value());
+    if (useCache()) {
+        QHashIterator<QString, QPixmap> it(pixmapsToCache);
+        while (it.hasNext()) {
+            it.next();
+            pixmapCache->insertPixmap(idsToCache[it.key()], it.value());
+        }
     }
 
     pixmapsToCache.clear();
