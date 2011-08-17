df4813ba2cd789f6dadb249d59214c067aa1a494
Aaron Seigo <aseigo@kde.org>
2011-08-17 18:07:12 +0200

    add missing smallest font support for qml and js widgets
    (cherry picked from commit 7a15d198c629fe1b642e727253e369bfdceb725c)
    (to fix compilation of kde-runtime with kdelibs master)

--- theme.h
+++ theme.h
@@ -78,7 +78,8 @@ class PLASMA_EXPORT Theme : public QObject
 
         enum FontRole {
             DefaultFont = 0, /**< The standard text font */
-            DesktopFont /**< The standard text font */
+            DesktopFont, /**< The standard text font */
+            SmallestFont /**< The smallest readable font */
         };
 
         /**
