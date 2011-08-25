79ad385f415504c13196c32fbad275c02a53ada2
Aaron Seigo <aseigo@kde.org>
2011-08-25 07:03:17 +0200

    give applets with no qwidget or graphicswidget a chance to do something

--- popupapplet.cpp
+++ popupapplet.cpp
@@ -429,7 +429,7 @@ void PopupAppletPrivate::popupConstraintsEvent(Plasma::Constraints constraints)
 
 void PopupAppletPrivate::appletActivated()
 {
-    internalTogglePopup();
+    internalTogglePopup(true);
 }
 
 QSizeF PopupApplet::sizeHint(Qt::SizeHint which, const QSizeF & constraint) const
@@ -702,7 +702,7 @@ void PopupAppletPrivate::iconSizeChanged(int group)
     }
 }
 
-void PopupAppletPrivate::internalTogglePopup()
+void PopupAppletPrivate::internalTogglePopup(bool fromActivatedSignal)
 {
     if (autohideTimer) {
         autohideTimer->stop();
@@ -713,6 +713,11 @@ void PopupAppletPrivate::internalTogglePopup()
     Plasma::Dialog *dialog = dialogPtr.data();
     if (!dialog) {
         q->setFocus(Qt::ShortcutFocusReason);
+        if (!fromActivatedSignal) {
+            QObject::disconnect(q, SIGNAL(activate()), q, SLOT(appletActivated()));
+            emit q->activate();
+            QObject::connect(q, SIGNAL(activate()), q, SLOT(appletActivated()));
+        }
         return;
     }
 
@@ -729,10 +734,15 @@ void PopupAppletPrivate::internalTogglePopup()
 
         dialog->clearFocus();
     } else {
-        if (!q->graphicsWidget() ||
-            (q->graphicsWidget() == static_cast<Applet*>(q)->d->extender.data() &&
-             static_cast<Applet*>(q)->d->extender.data()->isEmpty())) {
+        if (q->graphicsWidget() &&
+            q->graphicsWidget() == static_cast<Applet*>(q)->d->extender.data() &&
+            static_cast<Applet*>(q)->d->extender.data()->isEmpty()) {
             // we have nothing to show, so let's not.
+            if (!fromActivatedSignal) {
+                QObject::disconnect(q, SIGNAL(activate()), q, SLOT(appletActivated()));
+                emit q->activate();
+                QObject::connect(q, SIGNAL(activate()), q, SLOT(appletActivated()));
+            }
             return;
         }
 
