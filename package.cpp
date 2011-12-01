2313cc258bc23dfd8a2de6209ab46b89b670ecda
Jeremy Whiting <jpwhiting@kde.org>
2011-12-01 15:50:44 -0700

    KTar already supports xz and lzma files, also support them in Plasma::Package. BUG:287193

--- package.cpp
+++ package.cpp
@@ -509,7 +509,8 @@ bool Package::installPackage(const QString &package,
         if (mimetype->is("application/zip")) {
             archive = new KZip(package);
         } else if (mimetype->is("application/x-compressed-tar") ||
-                   mimetype->is("application/x-tar")|| mimetype->is("application/x-bzip-compressed-tar")) {
+                   mimetype->is("application/x-tar")|| mimetype->is("application/x-bzip-compressed-tar") ||
+                   mimetype->is("application/x-xz") || mimetype->is("application/x-lzma")) {
             archive = new KTar(package);
         } else {
             kWarning() << "Could not open package file, unsupported archive format:" << package << mimetype->name();
