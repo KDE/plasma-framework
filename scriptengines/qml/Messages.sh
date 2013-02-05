39f1048f1519e4e12484176fb90f236287e30f43
Marco Martin <notmart@gmail.com>
2013-02-05 20:28:50 +0100

    fix messages.sh

--- scriptengines/qml/Messages.sh
+++ scriptengines/qml/Messages.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-$XGETTEXT common/*.cpp dataengine/*.cpp plasmoid/*.cpp runner/*.cpp simplebindings/*.cpp -o $podir/plasma_scriptengine_qscript.pot
+$XGETTEXT common/*.cpp plasmoid/*.cpp declarative/*.cpp -o $podir/plasma_scriptengine_declarative.pot
