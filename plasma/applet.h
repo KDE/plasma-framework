dd00f217a56761ae653ac90458117e18355985d6
Marco Martin <notmart@gmail.com>
2013-02-11 18:28:53 +0100

    get rid of Q_PROPERTY and Q_INVOKABLE
    Applet * won't be directly exposed to qml anymore

--- plasma/applet.h
+++ plasma/applet.h
@@ -70,18 +70,6 @@ class Package;
 class PLASMA_EXPORT Applet : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(bool hasConfigurationInterface READ hasConfigurationInterface)
-    Q_PROPERTY(QString name READ name WRITE setName)
-    Q_PROPERTY(QString pluginName READ pluginName)
-    Q_PROPERTY(QString category READ category)
-    Q_PROPERTY(ImmutabilityType immutability READ immutability WRITE setImmutability)
-    Q_PROPERTY(bool hasFailedToLaunch READ hasFailedToLaunch WRITE setFailedToLaunch)
-    Q_PROPERTY(bool busy READ isBusy WRITE setBusy)
-    Q_PROPERTY(bool configurationRequired READ configurationRequired WRITE setConfigurationRequired)
-    Q_PROPERTY(bool shouldConserveResources READ shouldConserveResources)
-    Q_PROPERTY(uint id READ id)
-    Q_PROPERTY(BackgroundHints backgroundHints READ backgroundHints WRITE setBackgroundHints)
-    Q_PROPERTY(bool userConfiguring READ isUserConfiguring)
 
     public:
         typedef QList<Applet*> List;
@@ -159,7 +147,7 @@ class PLASMA_EXPORT Applet : public QObject
          *         or an invalid data engine if the requested engine
          *         could not be loaded
          */
-        Q_INVOKABLE DataEngine *dataEngine(const QString &name) const;
+        DataEngine *dataEngine(const QString &name) const;
 
         /**
          * Accessor for the associated Package object if any.
@@ -399,7 +387,7 @@ class PLASMA_EXPORT Applet : public QObject
         /**
          * Returns the QAction with the given name from our collection
          */
-        Q_INVOKABLE QAction *action(QString name) const;
+        QAction *action(QString name) const;
 
         /**
          * Adds the action to our collection under the given name
