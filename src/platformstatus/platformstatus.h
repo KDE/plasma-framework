e33bef8fdcf13f00a76f38e1e28b1157a3aaea0c
Ivan Čukić <ivan.cukic@kde.org>
2013-07-09 20:23:03 +0200

    Licensing information

--- src/platformstatus/platformstatus.h
+++ src/platformstatus/platformstatus.h
@@ -1,3 +1,23 @@
+/*
+ *   Copyright (C) 2013 by Aaron Seigo <aseigo@kde.org>
+ *   Copyright (C) 2013 by Ivan Cukic <ivan.cukic(at)kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2,
+ *   or (at your option) any later version, as published by the Free
+ *   Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 #ifndef PLATFORMSTATUS_H
 #define PLATFORMSTATUS_H
 
