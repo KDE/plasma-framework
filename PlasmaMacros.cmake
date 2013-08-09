9c345e5be5b467b63ae56ecb3744c743246e9d5e
Sebastian Kügler <sebas@kde.org>
2013-08-09 02:40:49 +0200

    plasma_add_plugin macro to simplify plugin installation

--- PlasmaMacros.cmake
+++ PlasmaMacros.cmake
@@ -14,3 +14,9 @@ macro(installPackage dir component)
    install(FILES ${dir}/metadata.desktop DESTINATION ${SERVICES_INSTALL_DIR} RENAME plasma-${type}-${component}.desktop)
 endmacro()
 
+macro(plasma_add_plugin plugin)
+    set(plugin_sources ${ARGN} )
+    add_library(${plugin} MODULE ${plugin_sources} )
+    set_target_properties(${plugin} PROPERTIES PREFIX "")
+endmacro()
+
