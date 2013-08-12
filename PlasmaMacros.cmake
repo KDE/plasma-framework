19d26505d6843581763ef92beac8a36657bde5c2
Sebastian Kügler <sebas@kde.org>
2013-08-13 01:15:36 +0200

    Docs for PlasmaMacros

--- PlasmaMacros.cmake
+++ PlasmaMacros.cmake
@@ -1,6 +1,14 @@
 
 find_package(KF5 MODULE REQUIRED COMPONENTS InstallDirs)
 
+
+# plasma_install_package(path componentname)
+#
+# Installs a Plasma package to the system path
+# @arg path The source path to install from, location of metadata.desktop
+# @arg componentname The plugin name of the component, corresponding to the
+#       X-KDE-PluginInfo-Name key in metadata.desktop
+#
 macro(plasma_install_package dir component)
    set(root ${ARGV2})
    set(type ${ARGV3})
@@ -14,6 +22,15 @@ macro(plasma_install_package dir component)
    install(FILES ${dir}/metadata.desktop DESTINATION ${SERVICES_INSTALL_DIR} RENAME plasma-${type}-${component}.desktop)
 endmacro()
 
+
+# plasma_add_plugin(pluginname sources_SRC)
+#
+# Use instead of add_library. Replacement for kde4_add_plugin
+# Basically does add_library and removes the prefix of the library
+#
+# @arg pluginname The name of the plugin,
+# @arg componentname The source files to be built
+#
 macro(plasma_add_plugin plugin)
     set(plugin_sources ${ARGN} )
     add_library(${plugin} MODULE ${plugin_sources} )
