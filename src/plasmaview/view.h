ff79662287fa48a81ec4f4e17aeaa0c34ac6b4bd
Giorgos Tsiapaliokas <terietor@gmail.com>
2013-08-29 14:59:11 +0300

    add apidocs for the View

--- src/plasmaview/view.h
+++ src/plasmaview/view.h
@@ -34,31 +34,82 @@ class PLASMAVIEW_EXPORT View : public QQuickView
     Q_PROPERTY(QRectF screenGeometry READ screenGeometry NOTIFY screenGeometryChanged)
 
 public:
+    /**
+     * @param corona the corona of this view
+     * @param parent the QWindow this View is parented to
+     **/
     explicit View(Plasma::Corona *corona, QWindow *parent = 0);
     virtual ~View();
 
+    /**
+     * @return the corona of this view
+     **/
     Plasma::Corona *corona() const;
 
+    /**
+     * @return the KConfigGroup of this view
+     **/
     virtual KConfigGroup config() const;
 
+    /**
+     * sets the containment for this view
+     * @param cont the containment of this view
+     **/
     virtual void setContainment(Plasma::Containment *cont);
+
+    /**
+     * @return the containment of this View
+     **/
     Plasma::Containment *containment() const;
+
+    /**
+     * @return the location of this View
+     **/
 
     //FIXME: Plasma::Types::Location should be something qml can understand
     int location() const;
+
+    /**
+     * Sets the location of the View
+     * @param location the location of the View
+     **/
     void setLocation(int location);
 
+    /**
+     * @return the formfactor of the View
+     **/
     Plasma::Types::FormFactor formFactor() const;
 
+    /**
+     * @return the screenGeometry of the View
+     **/
     QRectF screenGeometry();
 
 protected Q_SLOTS:
+    /**
+     * It will be called when the configuration is requested
+     */
     virtual void showConfigurationInterface(Plasma::Applet *applet);
 
 Q_SIGNALS:
+    /**
+     * emitted when the location is changed
+     **/
     void locationChanged(Plasma::Types::Location location);
+
+    /**
+     * emitted when the formfactor is changed
+     **/
     void formFactorChanged(Plasma::Types::FormFactor formFactor);
+
+    /**
+     * emitted when the containment is changed
+     **/
     void containmentChanged();
+
+    /**
+     * emitted when the screenGeometry is changed
+     **/
     void screenGeometryChanged();
 
 private:
