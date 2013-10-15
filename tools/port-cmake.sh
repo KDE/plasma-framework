aa2f0c787a31d5ab4a96581adeb778130e605890
Sebastian Kügler <sebas@kde.org>
2013-10-16 01:35:37 +0200

    update porting script

--- tools/port-cmake.sh
+++ tools/port-cmake.sh
@@ -11,12 +11,14 @@
 
 
 for FS in `find $PWD -type f -name 'CMakeLists.txt'`; do
-    perl -p -i -e 's/kde4_add_plugin/plasma_add_plugin/g' $FS
-    perl -p -i -e 's/kde4_add_ui_files/qt5_wrap_ui/g' $FS
-    perl -p -i -e 's/kde4_add_kdeinit_executable/kf5_add_kdeinit_executable/g' $FS
+    perl -p -i -e 's/KF5\:\:plasma/KF5\:\:Plasma/g' $FS
+#     perl -p -i -e 's/kde4_add_plugin/plasma_add_plugin/g' $FS
+#     perl -p -i -e 's/kde4_add_ui_files/qt5_wrap_ui/g' $FS
+#     perl -p -i -e 's/kde4_add_kdeinit_executable/kf5_add_kdeinit_executable/g' $FS
 
-    perl -p -i -e 's/KDE4_KIDLETIME_LIBRARY/KIdleTime_LIBRARIES/g' $FS
-    perl -p -i -e 's/KDE4_PLASMA_LIBS/KF5::plasma/g' $FS
+    perl -p -i -e 's/\$\{KDE4_KIDLETIME_LIBRARY\}/KF5::KIdleTime/g' $FS
+    perl -p -i -e 's/\$\{KDE4_PLASMA_LIBS\}/KF5::Plasma/g' $FS
+    perl -p -i -e 's/KF5\:\:plasma/KF5\:\:Plasma/g' $FS
     perl -p -i -e 's/\$\{KDE4_KDECORE_LIBS\}//g' $FS
     perl -p -i -e 's/\$\{KDE4_SOLID_LIBS\}/KF5\:\:Solid/g' $FS
     perl -p -i -e 's/\$\{KDE4_KIO_LIBS\}/KF5\:\:KIOCore\ KF5\:\:KIOWidgets/g' $FS
