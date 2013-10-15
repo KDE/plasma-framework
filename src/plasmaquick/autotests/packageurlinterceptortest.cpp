9f85a7dc119a1eeed3edac15e395e3636888afc9
Sebastian Kügler <sebas@kde.org>
2013-10-15 02:37:06 +0200

    Add basic test for url interceptor

---
+++ src/plasmaquick/autotests/packageurlinterceptortest.cpp
@@ -0,0 +1,40 @@
+/******************************************************************************
+*   Copyright 2013 Sebastian Kügler <sebas@kde.org>                           *
+*                                                                             *
+*   This library is free software; you can redistribute it and/or             *
+*   modify it under the terms of the GNU Library General Public               *
+*   License as published by the Free Software Foundation; either              *
+*   version 2 of the License, or (at your option) any later version.          *
+*                                                                             *
+*   This library is distributed in the hope that it will be useful,           *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU          *
+*   Library General Public License for more details.                          *
+*                                                                             *
+*   You should have received a copy of the GNU Library General Public License *
+*   along with this library; see the file COPYING.LIB.  If not, write to      *
+*   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,      *
+*   Boston, MA 02110-1301, USA.                                               *
+*******************************************************************************/
+
+#include "packageurlinterceptortest.h"
+
+#include <packageurlinterceptor.h>
+
+#include <QQmlNetworkAccessManagerFactory>
+
+#include <qtest.h>
+#include <QDebug>
+
+QTEST_MAIN(PackageUrlInterceptorTest)
+
+void PackageUrlInterceptorTest::loadAccessManager()
+{
+    Plasma::Package pkg = Plasma::Package();
+    QQmlNetworkAccessManagerFactory* pui = PackageUrlInterceptor::createPackageAccessManagerFactory(pkg);
+    QVERIFY(pui != 0);
+    delete pui;
+}
+
+#include "moc_packageurlinterceptortest.cpp"
+
