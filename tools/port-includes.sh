d1171a862de75c5aa3fa316a0be67ba004f2cbbe
Aleix Pol <aleixpol@kde.org>
2013-11-11 18:10:35 +0100

    Don't use KDE/ includes, they're deprecated

--- tools/port-includes.sh
+++ tools/port-includes.sh
@@ -4,4 +4,5 @@
 
 for FS in `find $PWD -type f -name '*.h' -or -name '*.cpp'`; do
     perl -p -i -e 's/#include\s+<Qt\w+\/(\w*)>/#include <\1>/g' $FS
+    perl -p -i -e 's/#include\s+<KDE\/(\w*)>/#include <\1>/g' $FS
 done
