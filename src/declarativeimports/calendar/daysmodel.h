33f2670f07bcafdfe31e18eb327f28c5f7d35f26
Martin Klapetek <mklapetek@kde.org>
2013-11-27 00:13:44 +0100

    Remove empty Q_SIGNALS & Q_SLOTS

--- src/declarativeimports/calendar/daysmodel.h
+++ src/declarativeimports/calendar/daysmodel.h
@@ -39,18 +39,15 @@ public:
         monthNumber,
         yearNumber
     };
+
     explicit DaysModel(QObject *parent = 0);
     void setSourceData(QList<DayData>* data);
     virtual int rowCount(const QModelIndex &parent) const;
     virtual QVariant data(const QModelIndex &index, int role) const;
     void update();
-    
-Q_SIGNALS:
-    
-public Q_SLOTS:
-    
+
 private:
-    QList<DayData>* m_data;
+    QList<DayData> *m_data;
 };
 
 #endif // DAYSMODEL_H
