d9a4c3e0eb33690636ec3e87a1b780938945a572
Aleix Pol <aleixpol@kde.org>
2013-12-14 16:38:48 +0100

    Adapt scripts to changes in kdelibs

--- tools/port-cmake.sh
+++ tools/port-cmake.sh
@@ -31,27 +31,27 @@ for FS in `find $PWD -type f -name 'CMakeLists.txt'`; do
     perl -p -i -e 's/macro_optional_add_subdirectory/ecm_optional_add_subdirectory/g' $FS
 
     perl -p -i -e 's/KF5\:\:plasma/KF5\:\:Plasma/g' $FS
-    perl -p -i -e 's/\$\{KDE4_KIDLETIME_LIBRARY\}/KF5::KIdleTime/g' $FS
+    perl -p -i -e 's/\$\{KDE4_KIDLETIME_LIBRARY\}/KF5::IdleTime/g' $FS
     perl -p -i -e 's/\$\{KDE4_PLASMA_LIBS\}/KF5::Plasma/g' $FS
     perl -p -i -e 's/\$\{KDE4_KDECORE_LIBS\}//g' $FS
     perl -p -i -e 's/\$\{KDE4_SOLID_LIBS\}/KF5\:\:Solid/g' $FS
     perl -p -i -e 's/\$\{KDE4_KIO_LIBS\}/KF5\:\:KIOWidgets/g' $FS
-    perl -p -i -e 's/\$\{KDE4_KNOTIFYCONFIG_LIBRARY\}/KF5\:\:KNotifyConfig/g' $FS
-    perl -p -i -e 's/\$\{KDE4_KNEWSTUFF3_LIBRARY\}/KF5\:\:KNewStuff/g' $FS
-    perl -p -i -e 's/\$\{KDE4_KNEWSTUFF3_LIBS\}/KF5\:\:KNewStuff/g' $FS
+    perl -p -i -e 's/\$\{KDE4_KNOTIFYCONFIG_LIBRARY\}/KF5\:\:NotifyConfig/g' $FS
+    perl -p -i -e 's/\$\{KDE4_KNEWSTUFF3_LIBRARY\}/KF5\:\KNewStuff/g' $FS
+    perl -p -i -e 's/\$\{KDE4_KNEWSTUFF3_LIBS\}/KF5\:\KNewStuff/g' $FS
     perl -p -i -e 's/\$\{KDE4_KDESU_LIBS\}/KF5\:\:KDESu/g' $FS
-    perl -p -i -e 's/\$\{KDE4_KTEXTEDITOR_LIBS\}/KF5\:\:KTextEditor/g' $FS
-    perl -p -i -e 's/\$\{KDE4_KPTY_LIBS\}/KF5\:\:KPty/g' $FS
+    perl -p -i -e 's/\$\{KDE4_KTEXTEDITOR_LIBS\}/KF5\:\:TextEditor/g' $FS
+    perl -p -i -e 's/\$\{KDE4_KPTY_LIBS\}/KF5\:\:Pty/g' $FS
     perl -p -i -e 's/\$\{Solid_LIBRARIES\}/KF5\:\:Solid/g' $FS
-    perl -p -i -e 's/\$\{KService_LIBRARIES\}/KF5\:\:KService/g' $FS
-    perl -p -i -e 's/\$\{KCoreAddons_LIBRARIES\}/KF5\:\:KCoreAddons/g' $FS
-    perl -p -i -e 's/\$\{KI18n_LIBRARIES\}/KF5\:\:KI18n/g' $FS
-    perl -p -i -e 's/\$\{KArchive_LIBRARIES\}/KF5\:\:KArchive/g' $FS
+    perl -p -i -e 's/\$\{KService_LIBRARIES\}/KF5\:\:Service/g' $FS
+    perl -p -i -e 's/\$\{KCoreAddons_LIBRARIES\}/KF5\:\:CoreAddons/g' $FS
+    perl -p -i -e 's/\$\{KI18n_LIBRARIES\}/KF5\:\:I18n/g' $FS
+    perl -p -i -e 's/\$\{KArchive_LIBRARIES\}/KF5\:\:Archive/g' $FS
     perl -p -i -e 's/\$\{Plasma_LIBRARIES\}/KF5\:\:Plasma/g' $FS
-    perl -p -i -e 's/\$\{KNotifications_LIBRARIES\}/KF5\:\:KNotifications/g' $FS
-    perl -p -i -e 's/\$\{KIconThemes_LIBRARIES\}/KF5\:\:KIconThemes/g' $FS
-    perl -p -i -e 's/\$\{KWindowSystem_LIBRARIES\}/KF5\:\:KWindowSystem/g' $FS
-    perl -p -i -e 's/\$\{KConfigWidgets_LIBRARIES\}/KF5\:\:KConfigWidgets/g' $FS
+    perl -p -i -e 's/\$\{KNotifications_LIBRARIES\}/KF5\:\:Notifications/g' $FS
+    perl -p -i -e 's/\$\{KIconThemes_LIBRARIES\}/KF5\:\:IconThemes/g' $FS
+    perl -p -i -e 's/\$\{KWindowSystem_LIBRARIES\}/KF5\:\:WindowSystem/g' $FS
+    perl -p -i -e 's/\$\{KConfigWidgets_LIBRARIES\}/KF5\:\:ConfigWidgets/g' $FS
     perl -p -i -e 's/\$\{KCMUTILS_LIBS\}/KF5\:\:KCMUtils/g' $FS
 
     perl -p -i -e 's/\$\{QT_QTXML_LIBRARY\}/Qt5\:\:Xml/g' $FS
