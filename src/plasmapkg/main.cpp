ac1aba0f2a40e39c2836097677ce6f2f8d72c239
Sebastian Kügler <sebas@kde.org>
2014-01-31 01:51:31 +0100

    Add -t shorthand to plasmapkg
    This restores backwards compatibility in arguments for theme
    installation.

--- src/plasmapkg/main.cpp
+++ src/plasmapkg/main.cpp
@@ -38,7 +38,7 @@ int main(int argc, char **argv)
     parser.setApplicationDescription(description);
     parser.addOption(QCommandLineOption(QStringList() << "h" << "hash", i18nc("Do not translate <path>", "Generate a SHA1 hash for the package at <path>"), "path"));
     parser.addOption(QCommandLineOption(QStringList() << "g" << "global", i18n("For install or remove, operates on packages installed for all users.")));
-    parser.addOption(QCommandLineOption(QStringList() << "type",
+    parser.addOption(QCommandLineOption(QStringList() << "t" << "type",
                 i18nc("theme, wallpaper, etc. are keywords, but they may be translated, as both versions "
                        "are recognized by the application "
                        "(if translated, should be same as messages with 'package type' context below)",
