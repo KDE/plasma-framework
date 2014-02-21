840a77b083a91300222c5c37bb51dc0d9b908b8b
Marco Martin <notmart@gmail.com>
2014-02-21 20:08:01 +0100

    remove other excess devicepixelratio
    Svg manages devicepixelration completely internally, multiplying any of its sizes by it makes it applied twice, therefore incorrect

--- src/declarativeimports/core/svgitem.cpp
+++ src/declarativeimports/core/svgitem.cpp
@@ -70,10 +70,10 @@ QSizeF SvgItem::naturalSize() const
     if (!m_svg) {
         return QSizeF();
     } else if (!m_elementID.isEmpty()) {
-        return m_svg.data()->elementSize(m_elementID) * floor(m_units.devicePixelRatio());
+        return m_svg.data()->elementSize(m_elementID);
     }
 
-    return m_svg.data()->size() * floor(m_units.devicePixelRatio());
+    return m_svg.data()->size();
 }
 
 
