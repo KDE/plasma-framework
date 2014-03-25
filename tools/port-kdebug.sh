0fb74f1ae718a0b88826a92854d9a3cfc18d50f9
Sebastian Kügler <sebas@kde.org>
2014-03-25 02:27:38 +0100

    kdebug porting script: enable the useful bits

--- tools/port-kdebug.sh
+++ tools/port-kdebug.sh
@@ -3,10 +3,12 @@
 # kDebug() becomes qDebug() in cpp files
 
 for FS in `find $PWD  -name '*.h' -o -name '*.cpp'`; do
-#     perl -p -i -e 's/\#include \<KDebug\>/\#include \<QDebug\>/g' $FS
-#     perl -p -i -e 's/\#include \<KDebug\>/\#include \<QDebug\>/g' $FS
-#     perl -p -i -e 's/\#include \<kdebug\.h\>/\#include \<QDebug\>/g' $FS
-#     perl -p -i -e 's/kDebug\(\)/qDebug()/g' $FS
+     perl -p -i -e 's/\#include \<KDebug\>/\#include \<QDebug\>/g' $FS
+     perl -p -i -e 's/\#include \<KDebug\>/\#include \<QDebug\>/g' $FS
+     perl -p -i -e 's/\#include \<KDebug\>/\#include \<QDebug\>/g' $FS
+     perl -p -i -e 's/\#include \<kdebug\.h\>/\#include \<QDebug\>/g' $FS
+     perl -p -i -e 's/\#include \"kdebug\.h\"/\#include \<QDebug\>/g' $FS
+     perl -p -i -e 's/kDebug\(\)/qDebug()/g' $FS
     perl -p -i -e 's/kWarning\(\)/qWarning()/g' $FS
 #
 #     perl -p -i -e 's/kDebug\(250\)/\/\/qDebug()/g' $FS
