7ce31b6b694fe20d10008c50cd46ca4504a0b770
Marco Martin <notmart@gmail.com>
2014-04-23 21:14:56 +0200

    correct the last commit

--- src/plasmaquick/dialog.cpp
+++ src/plasmaquick/dialog.cpp
@@ -131,7 +131,7 @@ void DialogPrivate::syncBorders()
 
     //Tooltips always have all the borders
     // floating windows have all borders
-    if (!(q->flags() & Qt::ToolTip) || location == Plasma::Types::Floating) {
+    if (!(q->flags() & Qt::ToolTip) && location != Plasma::Types::Floating) {
         if (q->x() <= avail.x() || location == Plasma::Types::LeftEdge) {
             borders = borders & ~Plasma::FrameSvg::LeftBorder;
         }
