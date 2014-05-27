aab2c13164e5dc66b06bdc617941cc6072120ed5
Yuri Chornoivan <yurchor@ukr.net>
2014-05-27 07:02:32 +0300

    Fix typos found by mvillarino

--- src/plasma/private/packages.cpp
+++ src/plasma/private/packages.cpp
@@ -258,14 +258,14 @@ void ShellPackage::initPackage(Plasma::Package *package)
     //Files
     //Default layout
     package->addFileDefinition("defaultlayout", "layout.js", i18n("Default layout file"));
-    package->addFileDefinition("defaults", "defaults", i18n("Default plugins for containments, containmentActions etc"));
+    package->addFileDefinition("defaults", "defaults", i18n("Default plugins for containments, containmentActions, etc."));
     package->setMimeTypes("defaultlayout", QStringList() << "application/javascript");
     package->setMimeTypes("defaults", QStringList() << "text/plain");
 
     //Applet furniture
     package->addFileDefinition("appleterror", "applet/AppletError.qml", i18n("Error message shown when an applet fails to load"));
     package->addFileDefinition("compactapplet", "applet/CompactApplet.qml", i18n("QML component that shows an applet in a popup"));
-    package->addFileDefinition("defaultcompactrepresentation", "applet/DefaultCompactRepresentation.qml", i18n("Compact representation of an applet when collapsed in a popup, for instance as an icon. applets can override this component."));
+    package->addFileDefinition("defaultcompactrepresentation", "applet/DefaultCompactRepresentation.qml", i18n("Compact representation of an applet when collapsed in a popup, for instance as an icon. Applets can override this component."));
 
     //Configuration
     package->addFileDefinition("appletconfigurationui", "configuration/AppletConfiguration.qml", i18n("QML component for the configuration dialog for applets"));
