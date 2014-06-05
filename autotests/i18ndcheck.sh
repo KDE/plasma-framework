d6b8ea9161e1705ee6219a4e8a7c635e33ea5af1
David Edmundson <kde@davidedmundson.co.uk>
2014-06-05 11:57:04 +0200

    Add a test that i18nd is used throughout

---
+++ autotests/i18ndcheck.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#If this test fails it means you are probably using i18n() in your QML code
+#This should be replaced by i18nd in order to for i18n to load the correct catalog
+
+#First arg should be the directory to check
+
+! find $1 -name '*.qml' | xargs grep 'i18n[^d]*('
+
