5f1abfbbe372beb6fef33bd993a3e772e80fb09a
Marco Martin <notmart@gmail.com>
2014-08-08 13:29:46 +0200

    add definition for a screenshot

--- src/plasmaquick/private/packages.cpp
+++ src/plasmaquick/private/packages.cpp
@@ -31,6 +31,7 @@
 void LookAndFeelPackage::initPackage(Plasma::Package *package)
 {
     // http://community.kde.org/Plasma/lookAndFeelPackage#
+    package->addFileDefinition("screenshot", "screenshot.png", i18n("Theme preview thumbnail"));
 
     package->setDefaultPackageRoot(PLASMA_RELATIVE_DATA_INSTALL_DIR "/look-and-feel/");
 
