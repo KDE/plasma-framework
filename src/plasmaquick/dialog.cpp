9796f043b771accfc8644c8919de91eb5f265182
Marco Martin <notmart@gmail.com>
2014-10-08 14:06:10 +0200

    Revert "set flags again on QEvent::Expose"
    This reverts commit 65ab514a2c34b2ddf3c34a9c8abac92f1e6879d3.

--- src/plasmaquick/dialog.cpp
+++ src/plasmaquick/dialog.cpp
@@ -930,10 +930,8 @@ void Dialog::setType(WindowType type)
 
     if (type == Dock) {
         KWindowSystem::setOnAllDesktops(winId(), true);
-        KWindowSystem::setState(winId(), NET::KeepAbove);
     } else {
         KWindowSystem::setOnAllDesktops(winId(), false);
-        KWindowSystem::clearState(winId(), NET::KeepAbove);
     }
 
     emit typeChanged();
@@ -993,19 +991,6 @@ bool Dialog::event(QEvent *event)
         d->updateVisibility(true);
     } else if (event->type() == QEvent::Hide) {
         d->updateVisibility(false);
-    }
-
-    if (event->type() == QEvent::Expose) {
-        KWindowSystem::setType(winId(), (NET::WindowType)d->type);
-
-        if (d->type == Dock) {
-            KWindowSystem::setOnAllDesktops(winId(), true);
-            KWindowSystem::setState(winId(), NET::KeepAbove);
-            raise();
-        } else {
-            KWindowSystem::setOnAllDesktops(winId(), false);
-            KWindowSystem::clearState(winId(), NET::KeepAbove);
-        }
     }
 
     const bool retval = QQuickWindow::event(event);
