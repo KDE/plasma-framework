2f0f7716e4dace5d6249dd50492debf05fae4730
Marco Martin <notmart@gmail.com>
2015-01-30 14:56:16 +0100

    default to AutoText as textformat
    yep, we don't have access to the proper enum :/
    
    Change-Id: I47bc0662e4204e5f510704dbece1f64a2d1517ce

--- src/declarativeimports/core/tooltip.cpp
+++ src/declarativeimports/core/tooltip.cpp
@@ -35,7 +35,7 @@ int ToolTip::s_dialogUsers  = 0;
 
 ToolTip::ToolTip(QQuickItem *parent)
     : QQuickItem(parent),
-      m_textFormat(0),
+      m_textFormat(2),
       m_tooltipsEnabledGlobally(false),
       m_location(Plasma::Types::Floating),
       m_containsMouse(false),
