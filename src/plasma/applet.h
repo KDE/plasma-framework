993301c80b1cb31f01cd86167249583ce93f6a1c
Marco Martin <notmart@gmail.com>
2015-03-03 16:35:15 +0100

    add missing K_EXPORT_PLASMA_APPLET_WITH_JSON
    both old and new notation for compatibility:
    same as DataEngine
    
    Change-Id: I96af8f2255a94e1c0bc86e55ebb420de721444eb

--- src/plasma/applet.h
+++ src/plasma/applet.h
@@ -636,4 +636,8 @@ private:
     K_PLUGIN_FACTORY(factory, registerPlugin<classname>();) \
     K_EXPORT_PLUGIN_VERSION(PLASMA_VERSION)
 
+#define K_EXPORT_PLASMA_APPLET_WITH_JSON(libname, classname, jsonFile) \
+    K_PLUGIN_FACTORY_WITH_JSON(factory, jsonFile, registerPlugin<classname>();) \
+    K_EXPORT_PLUGIN_VERSION(PLASMA_VERSION)
+
 #endif // multiple inclusion guard
