2750155af49dde56fc8e048c4b8594c90516cd78
Marco Martin <notmart@gmail.com>
2015-11-23 20:31:20 +0100

    a minimal example of a custom shell

---
+++ examples/shell/customcorona.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2015 Marco Martin <notmart@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+#ifndef CUSTOMCORONA_H
+#define CUSTOMCORONA_H
+
+#include <plasma/corona.h>
+#include "plasmaquick/containmentview.h"
+
+class CustomCorona : public Plasma::Corona
+{
+    Q_OBJECT
+
+public:
+    explicit CustomCorona(QObject * parent = 0);
+    QRect screenGeometry(int id) const;
+
+public Q_SLOTS:
+    void load();
+
+private:
+    PlasmaQuick::ContainmentView *m_view;
+};
+
+#endif
