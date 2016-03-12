KDE/plasma-framework

commit 344d04e2306ca2fdaf17269bc218f4fad065cbbf

[Icon Item] Don't animate from previous pixmap when it has been invisible
We have a lot of reusable singletons that are just hidden when not needed, such
as the OSD. IconItem, however, will always fade from the previous state, even if
the previous action happened minutes ago.

This fixes for example changing the volume and then later changing the brightness
where you could see the OSD animating from the volume icon.

REVIEW: 122859

autotests/iconitemtest.cpp
@@ -240,8 +240,6 @@ void IconItemTest::animation()
 
 void IconItemTest::animationAfterHide()
 {
-    QEXPECT_FAIL("", "Needs 'no animation after hide' patch", Abort);
-
     QQuickItem *item1 = createIconItem();
     QQuickItem *item2 = createIconItem();
     item1->setProperty("source", "user-away");

src/declarativeimports/core/iconitem.cpp
@@ -441,6 +441,16 @@ void IconItem::loadPixmap()
     update();
 }
 
+void IconItem::itemChange(ItemChange change, const ItemChangeData &value)
+{
+    if (change == ItemVisibleHasChanged && value.boolValue) {
+        // Clear pixmap to disable animation
+        m_iconPixmap = QPixmap();
+    }
+
+    QQuickItem::itemChange(change, value);
+}
+
 void IconItem::geometryChanged(const QRectF &newGeometry,
                                const QRectF &oldGeometry)
 {

src/declarativeimports/core/iconitem.h
@@ -129,6 +129,7 @@ public:
     void updatePolish() Q_DECL_OVERRIDE;
     QSGNode* updatePaintNode(QSGNode * oldNode, UpdatePaintNodeData * updatePaintNodeData) Q_DECL_OVERRIDE;
 
+    void itemChange(ItemChange change, const ItemChangeData &value) Q_DECL_OVERRIDE;
     void geometryChanged(const QRectF &newGeometry,
                          const QRectF &oldGeometry) Q_DECL_OVERRIDE;
 

commit fd4632230006a81c37a9ca2678b36fd49bd6c714

IconItem: Schedule pixmap update when window changes
QQuickItem::polish is not called when window changes.

Regression from 344d04e2306ca2fdaf17269bc218f4fad065cbbf

REVIEW: 127342

autotests/iconitemtest.cpp
@@ -410,5 +410,23 @@ void IconItemTest::animatingEnabledChange()
     QVERIFY(img1 != img2); // animation is running
 }
 
+void IconItemTest::windowChanged()
+{
+    QQuickItem *item = createIconItem();
+    item->setProperty("animated", false);
+    item->setProperty("source", "tst-plasma-framework-test-icon");
+    QImage img = grabImage(item);
+
+    QQuickView newView;
+    newView.setSource(QUrl::fromLocalFile(QFINDTESTDATA("data/view.qml")));
+    newView.show();
+    QTest::qWaitForWindowExposed(&newView);
+
+    item->setProperty("visible", false);
+    item->setParentItem(newView.rootObject());
+    item->setProperty("visible", true);
+    QCOMPARE(grabImage(item), img);
+}
+
 QTEST_MAIN(IconItemTest)
 

autotests/iconitemtest.h
@@ -52,6 +52,7 @@ private Q_SLOTS:
     void changeColorGroup();
     void animatingActiveChange();
     void animatingEnabledChange();
+    void windowChanged();
 
 private:
     QQuickItem *createIconItem();

src/declarativeimports/core/iconitem.cpp
@@ -69,6 +69,9 @@ IconItem::IconItem(QQuickItem *parent)
     connect(this, &QQuickItem::enabledChanged,
             this, &IconItem::enabledChanged);
 
+    connect(this, &QQuickItem::windowChanged,
+            this, &IconItem::schedulePixmapUpdate);
+
     //initialize implicit size to the Dialog size
     setImplicitWidth(KIconLoader::global()->currentSize(KIconLoader::Dialog));
     setImplicitHeight(KIconLoader::global()->currentSize(KIconLoader::Dialog));