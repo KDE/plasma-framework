98c721e031f62797d0e48e26e717c8b1245f8374
Eike Hein <hein@kde.org>
2016-08-01 03:57:22 +0900

    Fix build.

--- src/scriptengines/qml/plasmoid/appletinterface.cpp
+++ src/scriptengines/qml/plasmoid/appletinterface.cpp
@@ -758,7 +758,7 @@ bool AppletInterface::eventFilter(QObject *watched, QEvent *event)
             }
 
             QMenu *desktopMenu = new QMenu;
-            emit applet->contextualActionsAboutToShow();
+            emit applet()->contextualActionsAboutToShow();
             ci->addAppletActions(desktopMenu, applet(), event);
 
             if (!desktopMenu->isEmpty()) {
