354bd71296b5b01f1810a2dbe00a763853b2c974
Friedrich W. H. Kossebau <kossebau@kde.org>
2018-02-26 07:15:25 +0100

    Add template for Plasma wallpaper with QML extension
    Summary:
    Allows to quickly start a new wallpaper which also wants to use a custom QML
    extension.
    
    Reviewers: mart
    
    Reviewed By: mart
    
    Subscribers: #frameworks
    
    Tags: #frameworks
    
    Differential Revision: https://phabricator.kde.org/D10849

---
+++ templates/plasma-wallpaper-with-qml-extension/plugin/%{APPNAMELC}plugin.h
@@ -0,0 +1,33 @@
+/*
+ *   Copyright (C) %{CURRENT_YEAR} by %{AUTHOR} <%{EMAIL}>
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef %{APPNAMEUC}PLUGIN_H
+#define %{APPNAMEUC}PLUGIN_H
+
+#include <QQmlExtensionPlugin>
+
+class %{APPNAME}Plugin : public QQmlExtensionPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface")
+
+public:
+    void registerTypes(const char *uri) override;
+};
+
+#endif // %{APPNAMEUC}PLUGIN_H
