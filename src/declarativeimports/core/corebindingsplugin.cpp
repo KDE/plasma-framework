KDE/plasma-framework

commit 6c627ea044e9e68ccbc9f3ecfc3ae49cb0af0fee

Remove usage of context properties for theme and units

src/declarativeimports/core/corebindingsplugin.cpp
@@ -74,7 +74,7 @@ void CoreBindingsPlugin::registerTypes(const char *uri)
     Q_ASSERT(uri == QByteArray("org.kde.plasma.core"));
 
     qmlRegisterUncreatableType<Plasma::Types>(uri, 2, 0, "Types", {});
-    qmlRegisterUncreatableType<Units>(uri, 2, 0, "Units", {});
+    qmlRegisterSingletonType<Units>(uri, 2, 0, "Units", [=](QQmlEngine*, QJSEngine*) -> QObject* { return &Units::instance(); });
 
     qmlRegisterType<Plasma::Svg>(uri, 2, 0, "Svg");
     qmlRegisterType<Plasma::FrameSvg>(uri, 2, 0, "FrameSvg");
@@ -82,7 +82,7 @@ void CoreBindingsPlugin::registerTypes(const char *uri)
     qmlRegisterType<Plasma::FrameSvgItem>(uri, 2, 0, "FrameSvgItem");
 
     //qmlRegisterType<ThemeProxy>(uri, 2, 0, "Theme");
-    qmlRegisterUncreatableType<Plasma::QuickTheme>(uri, 2, 0, "Theme", QStringLiteral("It is not possible to instantiate Theme directly."));
+    qmlRegisterSingletonType<Plasma::QuickTheme>(uri, 2, 0, "Theme", [=](QQmlEngine* engine, QJSEngine*) -> QObject* { return new Plasma::QuickTheme(engine); });
     qmlRegisterType<ColorScope>(uri, 2, 0, "ColorScope");
 
     qmlRegisterType<Plasma::DataSource>(uri, 2, 0, "DataSource");

src/declarativeimports/calendar/qml/DayDelegate.qml
@@ -64,7 +64,7 @@ MouseArea {
         // this is needed here as the text is first rendered, counting with the default root.cellHeight
         // then root.cellHeight actually changes to whatever it should be, but the Label does not pick
         // it up after that, so we need to change it explicitly after the cell size changes
-        label.font.pixelSize = Math.max(theme.smallestFont.pixelSize, Math.floor(daysCalendar.cellHeight / 3))
+        label.font.pixelSize = Math.max(PlasmaCore.Theme.smallestFont.pixelSize, Math.floor(daysCalendar.cellHeight / 3))
     }
 
     Rectangle {
@@ -79,8 +79,8 @@ MouseArea {
                 0
             }
         }
-        Behavior on opacity { NumberAnimation { duration: units.shortDuration*2 } }
-        color: theme.textColor
+        Behavior on opacity { NumberAnimation { duration: PlasmaCore.Units.shortDuration*2 } }
+        color: PlasmaCore.Theme.textColor
     }
 
     Rectangle {
@@ -96,8 +96,8 @@ MouseArea {
             }
         }
         visible: !today
-        Behavior on opacity { NumberAnimation { duration: units.shortDuration*2 } }
-        color: theme.highlightColor
+        Behavior on opacity { NumberAnimation { duration: PlasmaCore.Units.shortDuration*2 } }
+        color: PlasmaCore.Theme.highlightColor
         z: todayRect.z - 1
     }
 
@@ -114,7 +114,7 @@ MouseArea {
         id: label
         anchors {
             fill: todayRect
-            margins: units.smallSpacing
+            margins: PlasmaCore.Units.smallSpacing
         }
         horizontalAlignment: Text.AlignHCenter
         verticalAlignment: Text.AlignVCenter
@@ -123,14 +123,14 @@ MouseArea {
         wrapMode: Text.NoWrap
         elide: Text.ElideRight
         fontSizeMode: Text.HorizontalFit
-        font.pixelSize: Math.max(theme.smallestFont.pixelSize, Math.floor(daysCalendar.cellHeight / 3))
+        font.pixelSize: Math.max(PlasmaCore.Theme.smallestFont.pixelSize, Math.floor(daysCalendar.cellHeight / 3))
         // Plasma component set point size, this code wants to set pixel size
         // Setting both results in a warning
         // -1 is an undocumented same as unset (see qquickvaluetypes)
         font.pointSize: -1
-        color: today ? theme.backgroundColor : theme.textColor
+        color: today ? PlasmaCore.Theme.backgroundColor : PlasmaCore.Theme.textColor
         Behavior on color {
-            ColorAnimation { duration: units.shortDuration * 2 }
+            ColorAnimation { duration: PlasmaCore.Units.shortDuration * 2 }
         }
     }
 }

src/declarativeimports/calendar/qml/DaysCalendar.qml
@@ -76,7 +76,7 @@ Item {
     Behavior on scale {
         id: scaleBehavior
         ScaleAnimator {
-            duration: units.longDuration
+            duration: PlasmaCore.Units.longDuration
         }
     }
 
@@ -93,7 +93,7 @@ Item {
             left: canvas.left
             right: canvas.right
         }
-        spacing: units.smallSpacing
+        spacing: PlasmaCore.Units.smallSpacing
 
         PlasmaExtras.Heading {
             id: heading
@@ -193,7 +193,7 @@ Item {
             ctx.reset();
             ctx.save();
             ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.strokeStyle = theme.textColor;
+            ctx.strokeStyle = PlasmaCore.Theme.textColor;
             ctx.lineWidth = root.borderWidth
             ctx.globalAlpha = 1.0;
 
@@ -284,7 +284,7 @@ Item {
                 verticalAlignment: Text.AlignVCenter
                 opacity: 0.4
                 text: modelData
-                font.pixelSize: Math.max(theme.smallestFont.pixelSize, daysCalendar.cellHeight / 3)
+                font.pixelSize: Math.max(PlasmaCore.Theme.smallestFont.pixelSize, daysCalendar.cellHeight / 3)
             }
         }
     }
@@ -313,7 +313,7 @@ Item {
                 width: daysCalendar.cellWidth
                 height: daysCalendar.cellHeight
                 text: Qt.locale(Qt.locale().uiLanguages[0]).dayName(((calendarBackend.firstDayOfWeek + index) % days.count), Locale.ShortFormat)
-                font.pixelSize: Math.max(theme.smallestFont.pixelSize, daysCalendar.cellHeight / 3)
+                font.pixelSize: Math.max(PlasmaCore.Theme.smallestFont.pixelSize, daysCalendar.cellHeight / 3)
                 opacity: 0.4
                 horizontalAlignment: Text.AlignHCenter
                 verticalAlignment: Text.AlignVCenter

src/declarativeimports/calendar/qml/MonthView.qml
@@ -185,21 +185,21 @@ PinchArea {
             pushTransition: StackViewTransition {
                 NumberAnimation {
                     target: exitItem
-                    duration: units.longDuration
+                    duration: PlasmaCore.Units.longDuration
                     property: "opacity"
                     from: 1
                     to: 0
                 }
                 NumberAnimation {
                     target: enterItem
-                    duration: units.longDuration
+                    duration: PlasmaCore.Units.longDuration
                     property: "opacity"
                     from: 0
                     to: 1
                 }
                 NumberAnimation {
                     target: enterItem
-                    duration: units.longDuration
+                    duration: PlasmaCore.Units.longDuration
                     property: "transformScale"
                     from: 1.5
                     to: 1
@@ -208,21 +208,21 @@ PinchArea {
             popTransition: StackViewTransition {
                 NumberAnimation {
                     target: exitItem
-                    duration: units.longDuration
+                    duration: PlasmaCore.Units.longDuration
                     property: "opacity"
                     from: 1
                     to: 0
                 }
                 NumberAnimation {
                     target: exitItem
-                    duration: units.longDuration
+                    duration: PlasmaCore.Units.longDuration
                     property: "transformScale"
                     // so no matter how much you scaled, it would still fly towards you
                     to: exitItem.transformScale * 1.5
                 }
                 NumberAnimation {
                     target: enterItem
-                    duration: units.longDuration
+                    duration: PlasmaCore.Units.longDuration
                     property: "opacity"
                     from: 0
                     to: 1

src/declarativeimports/plasmacomponents/qml/Button.qml
@@ -49,7 +49,7 @@ QtControls.Button {
      *
      * See also Qt documentation for font type.
      */
-    property font font: theme.defaultFont
+    property font font: PlasmaCore.Theme.defaultFont
 
     /*
      * overrides iconsource for compatibility

src/declarativeimports/plasmacomponents/qml/ButtonColumn.qml
@@ -80,7 +80,7 @@ Column {
      */
     property Item checkedButton;
 
-    spacing: theme.defaultFont.pointSize
+    spacing: PlasmaCore.Theme.defaultFont.pointSize
 
     Component.onCompleted: {
         Behavior.create(root, {direction: Qt.Vertical});

src/declarativeimports/plasmacomponents/qml/ButtonRow.qml
@@ -76,7 +76,7 @@ Row {
      */
     property Item checkedButton;
 
-    spacing: theme.defaultFont.pointSize
+    spacing: PlasmaCore.Theme.defaultFont.pointSize
 
     Component.onCompleted: {
         Behavior.create(root, {direction: Qt.Horizontal});

src/declarativeimports/plasmacomponents/qml/CommonDialog.qml
@@ -155,8 +155,8 @@ PlasmaComponents.Dialog {
 
         PlasmaCore.IconItem {
             id: titleAreaIcon
-            width: units.iconSizes.small
-            height: units.iconSizes.small
+            width: PlasmaCore.Units.iconSizes.small
+            height: PlasmaCore.Units.iconSizes.small
             source: titleIcon
             anchors.left: parent.left
             anchors.rightMargin: 4

commit b1345e9d302c80f779cdb2cf816404168d6b60da

Set cpp ownership on the units instance
Fixes common and frequent crashes introduced by 6c627ea044e9e68ccbc9f3ecfc3ae49cb0af0fee.
Otherwise the engine will try to delete it. The docs say:
 NOTE: A QObject singleton type instance returned from a singleton type provider
 is owned by the QML engine unless the object has explicit
 QQmlEngine::CppOwnership flag set.

src/declarativeimports/core/corebindingsplugin.cpp
@@ -74,7 +74,10 @@ void CoreBindingsPlugin::registerTypes(const char *uri)
     Q_ASSERT(uri == QByteArray("org.kde.plasma.core"));
 
     qmlRegisterUncreatableType<Plasma::Types>(uri, 2, 0, "Types", {});
-    qmlRegisterSingletonType<Units>(uri, 2, 0, "Units", [](QQmlEngine*, QJSEngine*) -> QObject* { return &Units::instance(); });
+    qmlRegisterSingletonType<Units>(uri, 2, 0, "Units", [](QQmlEngine *engine, QJSEngine*) -> QObject* {
+        engine->setObjectOwnership(&Units::instance(), QQmlEngine::CppOwnership);
+        return &Units::instance();
+    });
 
     qmlRegisterType<Plasma::Svg>(uri, 2, 0, "Svg");
     qmlRegisterType<Plasma::FrameSvg>(uri, 2, 0, "FrameSvg");