3b11e7546261cb72eb1becae2c18be530f70e4ef
Mikel Johnson <mikel5764@gmail.com>
2020-11-20 15:21:23 +0000

    Revert "[Dialog] Make BackgroundHints a flag and add DiscardBackgroundMargins"
    This reverts commit 4079eaae0e50f07419a1195665bbb0efdc9b7207

--- src/plasmaquick/dialog.cpp
+++ src/plasmaquick/dialog.cpp
@@ -486,15 +486,10 @@ void DialogPrivate::updateLayoutParameters()
         q->resize(finalSize);
     }
 
-    if (q->backgroundHints().testFlag(Dialog::DiscardBackgroundMargins)) {
-        mainItem->setPosition(QPointF(margin->left(),
+    mainItem->setPosition(QPointF(margin->left(),
                         margin->top()));
-        mainItem->setSize(QSizeF(q->width() - margin->left() - margin->right(),
+    mainItem->setSize(QSizeF(q->width() - margin->left() - margin->right(),
                         q->height() - margin->top() - margin->bottom()));
-    } else {
-        mainItem->setPosition(QPointF(0, 0));
-        mainItem->setSize(QSizeF(q->width(), q->height()));
-    }
 
     frameSvgItem->setSize(QSizeF(q->width(),
                             q->height()));
@@ -642,12 +637,8 @@ void DialogPrivate::syncToMainItemSize()
         q->resize(s);
     }
 
-    if (q->backgroundHints().testFlag(Dialog::DiscardBackgroundMargins)) {
-        mainItem->setPosition(QPointF(frameSvgItem->fixedMargins()->left(),
+    mainItem->setPosition(QPointF(frameSvgItem->fixedMargins()->left(),
                             frameSvgItem->fixedMargins()->top()));
-    } else {
-        mainItem->setPosition(QPointF(0, 0));
-    }
 
     updateTheme();
 }
@@ -665,15 +656,9 @@ void DialogPrivate::slotWindowPositionChanged()
 
     if (mainItem) {
         auto margin = frameSvgItem->fixedMargins();
-        if (q->backgroundHints().testFlag(Dialog::DiscardBackgroundMargins)) {
-            mainItem->setPosition(QPointF(margin->left(),
-                            margin->top()));
-            mainItem->setSize(QSizeF(q->width() - margin->left() - margin->right(),
-                            q->height() - margin->top() - margin->bottom()));
-        } else {
-            mainItem->setPosition(QPointF(0, 0));
-            mainItem->setSize(QSizeF(q->width(), q->height()));
-        }
+        mainItem->setPosition(QPoint(margin->left(), margin->top()));
+        mainItem->setSize(QSize(q->width() - margin->left() - margin->right(),
+                                q->height() - margin->top() - margin->bottom()));
     }
 }
 
@@ -1132,15 +1117,11 @@ void Dialog::resizeEvent(QResizeEvent* re)
     d->frameSvgItem->setSize(QSizeF(re->size().width(),
                                 re->size().height()));
     auto margin = d->frameSvgItem->fixedMargins();
-    if (backgroundHints().testFlag(Dialog::DiscardBackgroundMargins)) {
-        d->mainItem->setPosition(QPointF(margin->left(),
-                        margin->top()));
-        d->mainItem->setSize(QSizeF(re->size().width() - margin->left() - margin->right(),
-                        re->size().height() - margin->top() - margin->bottom()));
-    } else {
-        d->mainItem->setPosition(QPointF(0, 0));
-        d->mainItem->setSize(QSizeF(re->size().width(), re->size().height()));
-    }
+    d->mainItem->setPosition(QPointF(margin->left(),
+                                margin->top()));
+
+    d->mainItem->setSize(QSize(re->size().width() - margin->left() - margin->right(),
+            re->size().height() - margin->top() - margin->bottom()));
 
     QObject::connect(d->mainItem, SIGNAL(widthChanged()), this, SLOT(slotMainItemSizeChanged()));
     QObject::connect(d->mainItem, SIGNAL(heightChanged()), this, SLOT(slotMainItemSizeChanged()));
--- src/plasmaquick/dialog.h
+++ src/plasmaquick/dialog.h
@@ -150,13 +150,11 @@ public:
     };
     Q_ENUM(WindowType)
 
-    enum BackgroundHint {
+    enum BackgroundHints {
         NoBackground = 0,         /**< Not drawing a background under the applet, the dialog has its own implementation */
-        StandardBackground = 1,   /**< The standard background from the theme is drawn */
-        DiscardBackgroundMargins = 2   /**< Don't constrain dialogs main QML item within dialogs margins. */
+        StandardBackground = 1   /**< The standard background from the theme is drawn */
     };
-    Q_DECLARE_FLAGS(BackgroundHints, BackgroundHint)
-    Q_FLAG(BackgroundHints)
+    Q_ENUM(BackgroundHints)
 
     explicit Dialog(QQuickItem *parent = nullptr);
     ~Dialog() override;
@@ -245,8 +243,6 @@ private:
 
     Q_PRIVATE_SLOT(d, void slotMainItemSizeChanged())
 };
-
-Q_DECLARE_OPERATORS_FOR_FLAGS(Dialog::BackgroundHints)
 
 }
 
