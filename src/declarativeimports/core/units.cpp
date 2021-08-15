KDE/plasma-framework

commit 4edb830f53aac4833ece024df59441029b01fde6

Fix Units::roundToIconSize() returning incorrect sizes with non-Qt scaling
When not using Qt Scaling (testable by using the Wayland session or
setting `PLASMA_USE_QT_SCALING=1` on X11), all pixel sizes are unscaled.
E.g. a 22x22 SmallMedium icon must be expressed as 44x44 to Plasma,
because it is not aware of Qt's scaling.

The `Units::roundToIconSize()` function was failing to do this, thus
returning incorrectly small icon sizes. For example you would ask it
for the correct icon for a 44px physical area, expecting a SmallMedium
icon--but internally the function would would find that 44 is lower than
48 (unscaled Large size), and give you the next smallest size, which
is Medium. Unscaled Medium is 32px, which is equivalent to a 16px scaled
icon. So you would get a Small icon when you expected a SmallMedium one.

Due to quirks of how the icon sizes increase at the larger end, correct
results were accidentally returned for larger icon sizes. However small
and smallMedium icons were always incorrectly getting the next smallest
size instead, most commonly manifesting as ToolButtons and System Tray
icons being too small.

This commit fixes the problem by scaling all of the icon sizes
internally both for the purposes of comparison and returning a final
value--but only when not using Qt scaling.

BUG: 390147
BUG: 381794
FIXED-IN: 5.85

src/declarativeimports/core/units.cpp
@@ -136,33 +136,60 @@ QQmlPropertyMap *Units::iconSizeHints() const
 
 int Units::roundToIconSize(int size)
 {
-    /*Do *not* use devicePixelIconSize here, we want to use the sizes of the pixmaps of the smallest icons on the disk. And those are unaffected by dpi*/
+    // If not using Qt scaling (e.g. on X11 by default), manually scale all sizes
+    // according to the DPR because Qt hasn't done it for us. Otherwise all
+    // returned sizes are too small and icons are only the right size by pure
+    // chance for the larger sizes due to their large differences in size
+    qreal multiplier = 1.0;
+    QScreen *primary = QGuiApplication::primaryScreen();
+    if (primary) {
+        // Note that when using Qt scaling, the multiplier will always be 1
+        // because Qt will scale everything for us. This is good and intentional.
+        multiplier = bestIconScaleForDevicePixelRatio((qreal)primary->logicalDotsPerInchX() / (qreal)96);
+    }
+
     if (size <= 0) {
         return 0;
 
-    } else if (size < KIconLoader::SizeSmall) {
-        return KIconLoader::SizeSmall / 2;
+    } else if (size < KIconLoader::SizeSmall * multiplier) {
+        return qRound(KIconLoader::SizeSmall * multiplier);
 
-    } else if (size < KIconLoader::SizeSmallMedium) {
-        return KIconLoader::SizeSmall;
+    } else if (size < KIconLoader::SizeSmallMedium * multiplier) {
+        return qRound(KIconLoader::SizeSmall * multiplier);
 
-    } else if (size < KIconLoader::SizeMedium) {
-        return KIconLoader::SizeSmallMedium;
+    } else if (size < KIconLoader::SizeMedium * multiplier) {
+        return qRound(KIconLoader::SizeSmallMedium * multiplier);
 
-    } else if (size < KIconLoader::SizeLarge) {
-        return KIconLoader::SizeMedium;
+    } else if (size < KIconLoader::SizeLarge * multiplier) {
+        return qRound(KIconLoader::SizeMedium * multiplier);
 
-    } else if (size < KIconLoader::SizeHuge) {
-        return KIconLoader::SizeLarge;
+    } else if (size < KIconLoader::SizeHuge * multiplier) {
+        return qRound(KIconLoader::SizeLarge * multiplier);
 
-    } else if (size < KIconLoader::SizeEnormous) {
-        return KIconLoader::SizeHuge;
+    } else if (size < KIconLoader::SizeEnormous * multiplier) {
+        return qRound(KIconLoader::SizeHuge * multiplier);
 
     } else {
         return size;
     }
 }
 
+int Units::bestIconScaleForDevicePixelRatio(const int ratio)
+{
+    if (ratio < 1.5) {
+        return 1;
+    } else if (ratio < 2.0) {
+        return 1.5;
+    } else if (ratio < 2.5) {
+        return 2.0;
+    } else if (ratio < 3.0) {
+        return 2.5;
+    } else if (ratio < 3.5) {
+        return 3.0;
+    } else {
+        return ratio;
+    }
+}
 int Units::devicePixelIconSize(const int size) const
 {
     /* in kiconloader.h
@@ -180,20 +207,7 @@ int Units::devicePixelIconSize(const int size) const
     // and multiplies the global settings with the dpi ratio.
     const qreal ratio = devicePixelRatio();
 
-    if (ratio < 1.5) {
-        return size;
-    } else if (ratio < 2.0) {
-        return size * 1.5;
-    } else if (ratio < 2.5) {
-        return size * 2.0;
-    } else if (ratio < 3.0) {
-        return size * 2.5;
-    } else if (ratio < 3.5) {
-        return size * 3.0;
-    } else {
-        return size * ratio;
-    }
-    // FIXME: Add special casing for < 64 cases: align to kiconloader size
+    return size * bestIconScaleForDevicePixelRatio(ratio);
 }
 
 qreal Units::devicePixelRatio() const

src/declarativeimports/core/units.h
@@ -265,6 +265,14 @@ private:
 
     void updateDevicePixelRatio();
     void updateAnimationSpeed();
+
+    /**
+     * @return the best-looking icon scale for the given device pixel ratio
+     * Note that this function is only relevant when using Plasma scaling and
+     * when using Qt scaling, it always returns 1.
+     */
+    static int bestIconScaleForDevicePixelRatio(const int ratio);
+
     /**
      * @return The dpi-adjusted size for a given icon size
      */

commit 837bb6975f6a0661d211107823b7587808fc3d4e

Fix Plasma (non-Qt) icon scaling with integer scale factors
An embarrassing error in 4edb830f53aac4833ece024df59441029b01fde6
caused the function which returns an appropriate scale factor for icons
to both take and return integer values rather than floating point
values, preventing it from working properly with any scale factor other
than an integer scale factor (e.g. 200%). This commit fixes that issue.

src/declarativeimports/core/units.cpp
@@ -174,7 +174,7 @@ int Units::roundToIconSize(int size)
     }
 }
 
-int Units::bestIconScaleForDevicePixelRatio(const int ratio)
+qreal Units::bestIconScaleForDevicePixelRatio(const qreal ratio)
 {
     if (ratio < 1.5) {
         return 1;

src/declarativeimports/core/units.h
@@ -271,7 +271,7 @@ private:
      * Note that this function is only relevant when using Plasma scaling and
      * when using Qt scaling, it always returns 1.
      */
-    static int bestIconScaleForDevicePixelRatio(const int ratio);
+    static qreal bestIconScaleForDevicePixelRatio(const qreal ratio);
 
     /**
      * @return The dpi-adjusted size for a given icon size