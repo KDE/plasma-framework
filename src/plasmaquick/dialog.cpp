3847e55a0c5f8187a172d40d43ed33cf221c0387
Fushan Wen <qydwhotmail@gmail.com>
2023-01-03 22:10:50 +0800

    Revert "dialog: don't rely on parent property"
    This reverts commit 30ef5a7164696bc5a110c5a36d4f4507e182e19d.
    
    It breaks KRunner.

--- src/plasmaquick/dialog.cpp
+++ src/plasmaquick/dialog.cpp
@@ -78,7 +78,7 @@ public:
         , resizableEdges({})
         , overridingCursor(false)
         , appletInterface(nullptr)
-        , componentComplete(dialog->parent() != nullptr /* Not used in QML */)
+        , componentComplete(dialog->parent() == nullptr)
         , backgroundHints(Dialog::StandardBackground)
     {
     }
@@ -1157,20 +1157,22 @@ QPoint Dialog::popupPosition(QQuickItem *item, const QSize &size)
 {
     if (!item) {
         // If no item was specified try to align at the center of the parent view
-        QScreen *dialogScreen = parent() ? parent()->screen() : screen();
-        if (dialogScreen) {
+        QQuickItem *parentItem = qobject_cast<QQuickItem *>(parent());
+        if (parentItem) {
+            QScreen *screen = parentItem->window()->screen();
+
             switch (d->location) {
             case Plasma::Types::TopEdge:
-                return QPoint(dialogScreen->availableGeometry().center().x() - size.width() / 2, dialogScreen->availableGeometry().y());
+                return QPoint(screen->availableGeometry().center().x() - size.width() / 2, screen->availableGeometry().y());
             case Plasma::Types::LeftEdge:
-                return QPoint(dialogScreen->availableGeometry().x(), dialogScreen->availableGeometry().center().y() - size.height() / 2);
+                return QPoint(screen->availableGeometry().x(), screen->availableGeometry().center().y() - size.height() / 2);
             case Plasma::Types::RightEdge:
-                return QPoint(dialogScreen->availableGeometry().right() - size.width(), dialogScreen->availableGeometry().center().y() - size.height() / 2);
+                return QPoint(screen->availableGeometry().right() - size.width(), screen->availableGeometry().center().y() - size.height() / 2);
             case Plasma::Types::BottomEdge:
-                return QPoint(dialogScreen->availableGeometry().center().x() - size.width() / 2, dialogScreen->availableGeometry().bottom() - size.height());
+                return QPoint(screen->availableGeometry().center().x() - size.width() / 2, screen->availableGeometry().bottom() - size.height());
             // Default center in the screen
             default:
-                return dialogScreen->geometry().center() - QPoint(size.width() / 2, size.height() / 2);
+                return screen->geometry().center() - QPoint(size.width() / 2, size.height() / 2);
             }
         } else {
             return QPoint();
