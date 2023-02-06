974268c99df4dd09ce4910ca534d2aba228fb466
Nicolas Fella <nicolas.fella@gmx.de>
2023-02-06 01:02:48 +0100

    Remove 4->5 porting tools

--- src/tools/kf5-lib-prefix-rename-test/src/BackendConfig.cmake
+++
@@ -1,1 +0,0 @@
-target_link_libraries(backlighthelper Qt5::Core KF6::KAuth KF6::KI18n)
--- src/tools/port-includes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# it will remove the module prefix in Qt includes, it's usually a problem
-# since many classes have changed module between Qt4 and Qt5
-
-for FS in `find $PWD -type f -name '*.h' -or -name '*.cpp'`; do
-    perl -p -i -e 's/#include\s+<Qt\w+\/(\w*)>/#include <\1>/g' $FS
-    perl -p -i -e 's/#include\s+<KDE\/(\w*)>/#include <\1>/g' $FS
-done
