Sonic-DE/sonic-interface-libraries

Move popups away from Dialog, introducing new semantic window positioning
This merge requests introduces two new classes.

PlasmaPopupWindow: This is a Plasma Themed window positioned relatively
to another item in another window. Processing the move is batched and
happens in sync with the rendering.

TransientPlacementHint: The API for this class maps closely to the
wayland xdg_positioner. We specify the position semantically and using
code similar to kwin's position the window obeying relevant constraints.

For now on both platforms this then maps to a global co-ordarinte that
we move the window to, longer term the wayland side can use the
positioner directly.

These will both be re-used for Applet popups.

There are some minor behavioural differences introduced in this MR:
 - Popups are resized to the tooltip's implicitSize
 - ToolTipDialog loses all (hopefully) irrelevant properties from Dialog
- ToolTipDialog changes from having a "location" property to a
"popupDirection"

TooltipArea is untouched, so clients shouldn't have to change

src/declarativeimports/core/tooltipdialog.cpp
@@ -15,25 +15,55 @@
 #include <KWindowSystem>
 #include <plasmaquick/sharedqmlengine.h>
 
-ToolTipDialog::ToolTipDialog(QQuickItem *parent)
-    : Dialog(parent)
+#include "waylandintegration_p.h"
+
+ToolTipDialog::ToolTipDialog()
+    : PopupPlasmaWindow()
     , m_qmlObject(nullptr)
     , m_hideTimeout(-1)
     , m_interactive(false)
     , m_owner(nullptr)
 {
-    setLocation(Plasma::Types::Floating);
-    setType(Dialog::WindowType::Tooltip);
+    Qt::WindowFlags flags = Qt::WindowDoesNotAcceptFocus | Qt::WindowStaysOnTopHint;
+    if (KWindowSystem::isPlatformX11()) {
+        flags |= Qt::ToolTip | Qt::BypassWindowManagerHint;
+    } else {
+        flags |= Qt::FramelessWindowHint;
+        PlasmaWaylandShellIntegration::get(this)->setRole(QtWayland::org_kde_plasma_surface::role_tooltip);
+    }
+    setFlags(flags);
 
     m_showTimer = new QTimer(this);
     m_showTimer->setSingleShot(true);
     connect(m_showTimer, &QTimer::timeout, this, [this]() {
         setVisible(false);
     });
+
+    connect(this, &PlasmaQuick::PlasmaWindow::mainItemChanged, this, [this]() {
+        if (m_lastMainItem) {
+            disconnect(m_lastMainItem, &QQuickItem::implicitWidthChanged, this, &ToolTipDialog::updateSize);
+            disconnect(m_lastMainItem, &QQuickItem::implicitHeightChanged, this, &ToolTipDialog::updateSize);
+        }
+        m_lastMainItem = mainItem();
+
+        if (!mainItem()) {
+            return;
+        }
+        connect(mainItem(), &QQuickItem::implicitWidthChanged, this, &ToolTipDialog::updateSize);
+        connect(mainItem(), &QQuickItem::implicitHeightChanged, this, &ToolTipDialog::updateSize);
+        updateSize();
+    });
 }
 
 ToolTipDialog::~ToolTipDialog()
 {
+}
+
+void ToolTipDialog::updateSize()
+{
+    QSize popupSize = QSize(mainItem()->implicitWidth(), mainItem()->implicitHeight());
+    popupSize = popupSize.grownBy(margins());
+    resize(popupSize);
 }
 
 QQuickItem *ToolTipDialog::loadDefaultItem()
@@ -60,19 +90,14 @@ void ToolTipDialog::showEvent(QShowEvent *event)
 {
     keepalive();
 
-    Dialog::showEvent(event);
+    PlasmaQuick::PopupPlasmaWindow::showEvent(event);
 }
 
 void ToolTipDialog::hideEvent(QHideEvent *event)
 {
     m_showTimer->stop();
 
-    Dialog::hideEvent(event);
-}
-
-void ToolTipDialog::resizeEvent(QResizeEvent *re)
-{
-    Dialog::resizeEvent(re);
+    PlasmaQuick::PopupPlasmaWindow::hideEvent(event);
 }
 
 bool ToolTipDialog::event(QEvent *e)
@@ -85,13 +110,7 @@ bool ToolTipDialog::event(QEvent *e)
         dismiss();
     }
 
-    bool ret = Dialog::event(e);
-    Qt::WindowFlags flags = Qt::ToolTip | Qt::WindowDoesNotAcceptFocus | Qt::WindowStaysOnTopHint;
-    if (KWindowSystem::isPlatformX11()) {
-        flags = flags | Qt::BypassWindowManagerHint;
-    }
-    setFlags(flags);
-    return ret;
+    return PopupPlasmaWindow::event(e);
 }
 
 QObject *ToolTipDialog::owner() const
@@ -126,7 +145,6 @@ bool ToolTipDialog::interactive()
 void ToolTipDialog::setInteractive(bool interactive)
 {
     m_interactive = interactive;
-    setOutputOnly(!interactive);
 }
 
 void ToolTipDialog::valueChanged(const QVariant &value)

src/plasmaquick/transientplacementhint.cpp
@@ -1,0 +1,229 @@
+/*
+    SPDX-FileCopyrightText: 2023 David Edmundson <davidedmundson@kde.org>
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "transientplacementhint_p.h"
+#include <QSharedData>
+
+#include <QDebug>
+#include <QGuiApplication>
+#include <QScreen>
+#include <QWindow>
+
+// This class is proposed for Qt6.something, but it's not there yet.
+// keep as an implementation detail, and then drop eventually (famous last words)
+
+class TransientPlacementHintPrivate : public QSharedData
+{
+public:
+    QRect parentAnchorRect;
+    Qt::Edges parentAnchor = Qt::BottomEdge | Qt::RightEdge;
+    Qt::Edges popupAnchor = Qt::TopEdge | Qt::LeftEdge;
+};
+/*!
+ * Constructs a new QTransientPlacementHint
+ */
+TransientPlacementHint::TransientPlacementHint()
+    : d(new TransientPlacementHintPrivate)
+{
+}
+
+TransientPlacementHint::~TransientPlacementHint()
+{
+}
+
+TransientPlacementHint::TransientPlacementHint(const TransientPlacementHint &other)
+{
+    d = other.d;
+}
+TransientPlacementHint &TransientPlacementHint::operator=(const TransientPlacementHint &other)
+{
+    d = other.d;
+    return *this;
+}
+
+bool TransientPlacementHint::isValid() const
+{
+    return d->parentAnchorRect.isValid();
+}
+
+void TransientPlacementHint::setParentAnchorArea(const QRect &parentAnchorRect)
+{
+    d->parentAnchorRect = parentAnchorRect;
+}
+
+QRect TransientPlacementHint::parentAnchorArea() const
+{
+    return d->parentAnchorRect;
+}
+
+void TransientPlacementHint::setParentAnchor(Qt::Edges parentAnchor)
+{
+    d->parentAnchor = parentAnchor;
+}
+
+Qt::Edges TransientPlacementHint::parentAnchor() const
+{
+    return d->parentAnchor;
+}
+
+void TransientPlacementHint::setPopupAnchor(Qt::Edges popupAnchor)
+{
+    d->popupAnchor = popupAnchor;
+}
+
+Qt::Edges TransientPlacementHint::popupAnchor() const
+{
+    return d->popupAnchor;
+}
+
+Qt::Orientations TransientPlacementHint::slideConstraintAdjustments() const
+{
+    return Qt::Horizontal | Qt::Vertical;
+}
+
+Qt::Orientations TransientPlacementHint::flipConstraintAdjustments() const
+{
+    // don't flip
+    return {};
+}
+
+static QPoint popupPosition(const QRect &anchorRect, const Qt::Edges parentAnchor, const Qt::Edges popupAnchor, const QSize &popupSize)
+{
+    QPoint anchorPoint;
+    switch (parentAnchor & (Qt::LeftEdge | Qt::RightEdge)) {
+    case Qt::LeftEdge:
+        anchorPoint.setX(anchorRect.x());
+        break;
+    case Qt::RightEdge:
+        anchorPoint.setX(anchorRect.x() + anchorRect.width());
+        break;
+    default:
+        anchorPoint.setX(qRound(anchorRect.x() + anchorRect.width() / 2.0));
+    }
+    switch (parentAnchor & (Qt::TopEdge | Qt::BottomEdge)) {
+    case Qt::TopEdge:
+        anchorPoint.setY(anchorRect.y());
+        break;
+    case Qt::BottomEdge:
+        anchorPoint.setY(anchorRect.y() + anchorRect.height());
+        break;
+    default:
+        anchorPoint.setY(qRound(anchorRect.y() + anchorRect.height() / 2.0));
+    }
+    // calculate where the top left point of the popup will end up with the applied popup anchor
+    QPoint popupPosAdjust;
+    switch (popupAnchor & (Qt::LeftEdge | Qt::RightEdge)) {
+    case Qt::LeftEdge:
+        popupPosAdjust.setX(0);
+        break;
+    case Qt::RightEdge:
+        popupPosAdjust.setX(-popupSize.width());
+        break;
+    default:
+        popupPosAdjust.setX(qRound(-popupSize.width() / 2.0));
+    }
+    switch (popupAnchor & (Qt::TopEdge | Qt::BottomEdge)) {
+    case Qt::TopEdge:
+        popupPosAdjust.setY(0);
+        break;
+    case Qt::BottomEdge:
+        popupPosAdjust.setY(-popupSize.height());
+        break;
+    default:
+        popupPosAdjust.setY(qRound(-popupSize.height() / 2.0));
+    }
+    return anchorPoint + popupPosAdjust;
+}
+
+QRect TransientPlacementHelper::popupRect(QWindow *w, const TransientPlacementHint &placement)
+{
+    Q_ASSERT(placement.isValid());
+    QScreen *screen = nullptr;
+    QRect globalParentAnchorRect = placement.parentAnchorArea();
+    if (w->transientParent()) {
+        globalParentAnchorRect = globalParentAnchorRect.translated(w->transientParent()->position());
+        screen = w->transientParent()->screen();
+    }
+    QRect popupRect = QRect(popupPosition(globalParentAnchorRect, placement.parentAnchor(), placement.popupAnchor(), w->size()), w->size());
+    if (!screen)
+        screen = qApp->screenAt(globalParentAnchorRect.center());
+    if (!screen)
+        screen = qApp->primaryScreen();
+    const QRect screenArea = screen->availableGeometry();
+    auto inScreenArea = [screenArea](const QRect &target, Qt::Edges edges = Qt::LeftEdge | Qt::RightEdge | Qt::TopEdge | Qt::BottomEdge) -> bool {
+        if (edges & Qt::LeftEdge && target.left() < screenArea.left()) {
+            return false;
+        }
+        if (edges & Qt::TopEdge && target.top() < screenArea.top()) {
+            return false;
+        }
+        if (edges & Qt::RightEdge && target.right() > screenArea.right()) {
+            return false;
+        }
+        if (edges & Qt::BottomEdge && target.bottom() > screenArea.bottom()) {
+            return false;
+        }
+        return true;
+    };
+
+    // if that fits, we don't need to do anything
+    if (inScreenArea(popupRect)) {
+        return popupRect;
+    }
+    // Otherwise,
+    if (placement.flipConstraintAdjustments() & Qt::Horizontal) {
+        if (!inScreenArea(popupRect, Qt::LeftEdge | Qt::RightEdge)) {
+            // flip both edges (if either bit is set, XOR both)
+            auto flippedParentAnchor = placement.parentAnchor();
+            if (flippedParentAnchor & (Qt::LeftEdge | Qt::RightEdge)) {
+                flippedParentAnchor ^= (Qt::LeftEdge | Qt::RightEdge);
+            }
+            auto flippedPopupAnchor = placement.popupAnchor();
+            if (flippedPopupAnchor & (Qt::LeftEdge | Qt::RightEdge)) {
+                flippedPopupAnchor ^= (Qt::LeftEdge | Qt::RightEdge);
+            }
+            QRect flippedPopupRect = QRect(popupPosition(globalParentAnchorRect, flippedParentAnchor, flippedPopupAnchor, w->size()), w->size());
+            // if it still doesn't fit we should continue with the unflipped version
+            if (inScreenArea(flippedPopupRect, Qt::LeftEdge | Qt::RightEdge)) {
+                popupRect.moveLeft(flippedPopupRect.left());
+            }
+        }
+    }
+    if (placement.slideConstraintAdjustments() & Qt::Horizontal) {
+        if (!inScreenArea(popupRect, Qt::LeftEdge)) {
+            popupRect.moveLeft(screenArea.left());
+        }
+        if (!inScreenArea(popupRect, Qt::RightEdge)) {
+            popupRect.moveRight(screenArea.right());
+        }
+    }
+    if (placement.flipConstraintAdjustments() & Qt::Vertical) {
+        if (!inScreenArea(popupRect, Qt::TopEdge | Qt::BottomEdge)) {
+            // flip both edges (if either bit is set, XOR both)
+            auto flippedParentAnchor = placement.parentAnchor();
+            if (flippedParentAnchor & (Qt::TopEdge | Qt::BottomEdge)) {
+                flippedParentAnchor ^= (Qt::TopEdge | Qt::BottomEdge);
+            }
+            auto flippedPopupAnchor = placement.popupAnchor();
+            if (flippedPopupAnchor & (Qt::TopEdge | Qt::BottomEdge)) {
+                flippedPopupAnchor ^= (Qt::TopEdge | Qt::BottomEdge);
+            }
+            QRect flippedPopupRect = QRect(popupPosition(globalParentAnchorRect, flippedParentAnchor, flippedPopupAnchor, w->size()), w->size());
+            // if it still doesn't fit we should continue with the unflipped version
+            if (inScreenArea(flippedPopupRect, Qt::TopEdge | Qt::BottomEdge)) {
+                popupRect.moveTop(flippedPopupRect.top());
+            }
+        }
+    }
+    if (placement.slideConstraintAdjustments() & Qt::Vertical) {
+        if (!inScreenArea(popupRect, Qt::TopEdge)) {
+            popupRect.moveTop(screenArea.top());
+        }
+        if (!inScreenArea(popupRect, Qt::BottomEdge)) {
+            popupRect.moveBottom(screenArea.bottom());
+        }
+    }
+    return popupRect;
+}

src/plasmaquick/transientplacementhint_p.h
@@ -1,0 +1,43 @@
+/*
+    SPDX-FileCopyrightText: 2023 David Edmundson <davidedmundson@kde.org>
+    SPDX-FileCopyrightText: 2021 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QRect>
+#include <QSharedDataPointer>
+#include <QtGlobal>
+
+// This class is proposed for Qt6.something, but it's not there yet.
+// keep as an implementation detail, and then drop eventually (famous last words)
+
+class TransientPlacementHintPrivate;
+class QWindow;
+
+class TransientPlacementHint
+{
+public:
+    TransientPlacementHint();
+    ~TransientPlacementHint();
+    TransientPlacementHint(const TransientPlacementHint &other);
+    TransientPlacementHint &operator=(const TransientPlacementHint &other);
+    bool isValid() const;
+    void setParentAnchorArea(const QRect &parentAnchorRect);
+    QRect parentAnchorArea() const;
+    void setParentAnchor(Qt::Edges parentAnchor);
+    Qt::Edges parentAnchor() const;
+    void setPopupAnchor(Qt::Edges popupAnchor);
+    Qt::Edges popupAnchor() const;
+    Qt::Orientations slideConstraintAdjustments() const;
+    Qt::Orientations flipConstraintAdjustments() const;
+
+private:
+    QSharedDataPointer<TransientPlacementHintPrivate> d;
+};
+
+namespace TransientPlacementHelper
+{
+QRect popupRect(QWindow *window, const TransientPlacementHint &placement);
+}

src/plasmaquick/waylandintegration_p.h
@@ -13,6 +13,8 @@
 
 #include "qwayland-plasma-shell.h"
 
+#include <plasmaquick/plasmaquick_export.h>
+
 class QWindow;
 
 class PlasmaShellSurface;
@@ -24,7 +26,7 @@ class PlasmaShellSurface;
  * The class abstracts the wayland protocol tasks, automatically sending
  * cached metadata when the underlying platform surfaces are created.
  */
-class PlasmaWaylandShellIntegration : public QObject
+class PLASMAQUICK_EXPORT PlasmaWaylandShellIntegration : public QObject
 {
     Q_OBJECT
 public:

KDE/plasma-framework

src/declarativeimports/core/tooltipdialog.cpp
@@ -15,25 +15,55 @@
 #include <KWindowSystem>
 #include <plasmaquick/sharedqmlengine.h>
 
-ToolTipDialog::ToolTipDialog(QQuickItem *parent)
-    : Dialog(parent)
+#include "waylandintegration_p.h"
+
+ToolTipDialog::ToolTipDialog()
+    : PopupPlasmaWindow()
     , m_qmlObject(nullptr)
     , m_hideTimeout(-1)
     , m_interactive(false)
     , m_owner(nullptr)
 {
-    setLocation(Plasma::Types::Floating);
-    setType(Dialog::WindowType::Tooltip);
+    Qt::WindowFlags flags = Qt::WindowDoesNotAcceptFocus | Qt::WindowStaysOnTopHint;
+    if (KWindowSystem::isPlatformX11()) {
+        flags |= Qt::ToolTip | Qt::BypassWindowManagerHint;
+    } else {
+        flags |= Qt::FramelessWindowHint;
+        PlasmaShellWaylandIntegration::get(this)->setRole(QtWayland::org_kde_plasma_surface::role_tooltip);
+    }
+    setFlags(flags);
 
     m_showTimer = new QTimer(this);
     m_showTimer->setSingleShot(true);
     connect(m_showTimer, &QTimer::timeout, this, [this]() {
         setVisible(false);
     });
+
+    connect(this, &PlasmaQuick::PlasmaWindow::mainItemChanged, this, [this]() {
+        if (m_lastMainItem) {
+            disconnect(m_lastMainItem, &QQuickItem::implicitWidthChanged, this, &ToolTipDialog::updateSize);
+            disconnect(m_lastMainItem, &QQuickItem::implicitHeightChanged, this, &ToolTipDialog::updateSize);
+        }
+        m_lastMainItem = mainItem();
+
+        if (!mainItem()) {
+            return;
+        }
+        connect(mainItem(), &QQuickItem::implicitWidthChanged, this, &ToolTipDialog::updateSize);
+        connect(mainItem(), &QQuickItem::implicitHeightChanged, this, &ToolTipDialog::updateSize);
+        updateSize();
+    });
 }
 
 ToolTipDialog::~ToolTipDialog()
 {
+}
+
+void ToolTipDialog::updateSize()
+{
+    QSize popupSize = QSize(mainItem()->implicitWidth(), mainItem()->implicitHeight());
+    popupSize = popupSize.grownBy(margins());
+    resize(popupSize);
 }
 
 QQuickItem *ToolTipDialog::loadDefaultItem()
@@ -53,19 +83,14 @@ void ToolTipDialog::showEvent(QShowEvent *event)
 {
     keepalive();
 
-    Dialog::showEvent(event);
+    PlasmaQuick::PopupPlasmaWindow::showEvent(event);
 }
 
 void ToolTipDialog::hideEvent(QHideEvent *event)
 {
     m_showTimer->stop();
 
-    Dialog::hideEvent(event);
-}
-
-void ToolTipDialog::resizeEvent(QResizeEvent *re)
-{
-    Dialog::resizeEvent(re);
+    PlasmaQuick::PopupPlasmaWindow::hideEvent(event);
 }
 
 bool ToolTipDialog::event(QEvent *e)
@@ -78,13 +103,7 @@ bool ToolTipDialog::event(QEvent *e)
         dismiss();
     }
 
-    bool ret = Dialog::event(e);
-    Qt::WindowFlags flags = Qt::ToolTip | Qt::WindowDoesNotAcceptFocus | Qt::WindowStaysOnTopHint;
-    if (KWindowSystem::isPlatformX11()) {
-        flags = flags | Qt::BypassWindowManagerHint;
-    }
-    setFlags(flags);
-    return ret;
+    return PopupPlasmaWindow::event(e);
 }
 
 QObject *ToolTipDialog::owner() const
@@ -119,7 +138,6 @@ bool ToolTipDialog::interactive()
 void ToolTipDialog::setInteractive(bool interactive)
 {
     m_interactive = interactive;
-    setOutputOnly(!interactive);
 }
 
 void ToolTipDialog::valueChanged(const QVariant &value)

src/plasmaquick/transientplacementhint.cpp
@@ -1,0 +1,240 @@
+/*
+    SPDX-FileCopyrightText: 2023 David Edmundson <davidedmundson@kde.org>
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "transientplacementhint_p.h"
+#include <QSharedData>
+
+#include <QDebug>
+#include <QGuiApplication>
+#include <QScreen>
+#include <QWindow>
+
+// This class is proposed for Qt6.something, but it's not there yet.
+// keep as an implementation detail, and then drop eventually (famous last words)
+
+class TransientPlacementHintPrivate : public QSharedData
+{
+public:
+    QRect parentAnchorRect;
+    Qt::Edges parentAnchor = Qt::BottomEdge | Qt::RightEdge;
+    Qt::Edges popupAnchor = Qt::TopEdge | Qt::LeftEdge;
+    Qt::Orientations slideConstraintAdjustments = Qt::Horizontal | Qt::Vertical;
+    Qt::Orientations flipConstraintAdjustments;
+};
+/*!
+ * Constructs a new QTransientPlacementHint
+ */
+TransientPlacementHint::TransientPlacementHint()
+    : d(new TransientPlacementHintPrivate)
+{
+}
+
+TransientPlacementHint::~TransientPlacementHint()
+{
+}
+
+TransientPlacementHint::TransientPlacementHint(const TransientPlacementHint &other)
+{
+    d = other.d;
+}
+TransientPlacementHint &TransientPlacementHint::operator=(const TransientPlacementHint &other)
+{
+    d = other.d;
+    return *this;
+}
+
+bool TransientPlacementHint::isValid() const
+{
+    return d->parentAnchorRect.isValid();
+}
+
+void TransientPlacementHint::setParentAnchorArea(const QRect &parentAnchorRect)
+{
+    d->parentAnchorRect = parentAnchorRect;
+}
+
+QRect TransientPlacementHint::parentAnchorArea() const
+{
+    return d->parentAnchorRect;
+}
+
+void TransientPlacementHint::setParentAnchor(Qt::Edges parentAnchor)
+{
+    d->parentAnchor = parentAnchor;
+}
+
+Qt::Edges TransientPlacementHint::parentAnchor() const
+{
+    return d->parentAnchor;
+}
+
+void TransientPlacementHint::setPopupAnchor(Qt::Edges popupAnchor)
+{
+    d->popupAnchor = popupAnchor;
+}
+
+Qt::Edges TransientPlacementHint::popupAnchor() const
+{
+    return d->popupAnchor;
+}
+
+void TransientPlacementHint::setSlideConstraintAdjustments(Qt::Orientations slideConstraintAdjustments)
+{
+    d->slideConstraintAdjustments = slideConstraintAdjustments;
+}
+
+Qt::Orientations TransientPlacementHint::slideConstraintAdjustments() const
+{
+    return d->slideConstraintAdjustments;
+}
+
+void TransientPlacementHint::setFlipConstraintAdjustments(Qt::Orientations flipConstraintAdjustments)
+{
+    d->flipConstraintAdjustments = flipConstraintAdjustments;
+}
+
+Qt::Orientations TransientPlacementHint::flipConstraintAdjustments() const
+{
+    return d->flipConstraintAdjustments;
+}
+
+static QPoint popupPosition(const QRect &anchorRect, const Qt::Edges parentAnchor, const Qt::Edges popupAnchor, const QSize &popupSize)
+{
+    QPoint anchorPoint;
+    switch (parentAnchor & (Qt::LeftEdge | Qt::RightEdge)) {
+    case Qt::LeftEdge:
+        anchorPoint.setX(anchorRect.x());
+        break;
+    case Qt::RightEdge:
+        anchorPoint.setX(anchorRect.x() + anchorRect.width());
+        break;
+    default:
+        anchorPoint.setX(qRound(anchorRect.x() + anchorRect.width() / 2.0));
+    }
+    switch (parentAnchor & (Qt::TopEdge | Qt::BottomEdge)) {
+    case Qt::TopEdge:
+        anchorPoint.setY(anchorRect.y());
+        break;
+    case Qt::BottomEdge:
+        anchorPoint.setY(anchorRect.y() + anchorRect.height());
+        break;
+    default:
+        anchorPoint.setY(qRound(anchorRect.y() + anchorRect.height() / 2.0));
+    }
+    // calculate where the top left point of the popup will end up with the applied popup anchor
+    QPoint popupPosAdjust;
+    switch (popupAnchor & (Qt::LeftEdge | Qt::RightEdge)) {
+    case Qt::LeftEdge:
+        popupPosAdjust.setX(0);
+        break;
+    case Qt::RightEdge:
+        popupPosAdjust.setX(-popupSize.width());
+        break;
+    default:
+        popupPosAdjust.setX(qRound(-popupSize.width() / 2.0));
+    }
+    switch (popupAnchor & (Qt::TopEdge | Qt::BottomEdge)) {
+    case Qt::TopEdge:
+        popupPosAdjust.setY(0);
+        break;
+    case Qt::BottomEdge:
+        popupPosAdjust.setY(-popupSize.height());
+        break;
+    default:
+        popupPosAdjust.setY(qRound(-popupSize.height() / 2.0));
+    }
+    return anchorPoint + popupPosAdjust;
+}
+
+QRect TransientPlacementHelper::popupRect(QWindow *w, const TransientPlacementHint &placement)
+{
+    Q_ASSERT(placement.isValid());
+    QScreen *screen = nullptr;
+    QRect globalParentAnchorRect = placement.parentAnchorArea();
+    if (w->transientParent()) {
+        globalParentAnchorRect = globalParentAnchorRect.translated(w->transientParent()->position());
+        screen = w->transientParent()->screen();
+    }
+    QRect popupRect = QRect(popupPosition(globalParentAnchorRect, placement.parentAnchor(), placement.popupAnchor(), w->size()), w->size());
+    if (!screen)
+        screen = qApp->screenAt(globalParentAnchorRect.center());
+    if (!screen)
+        screen = qApp->primaryScreen();
+    const QRect screenArea = screen->availableGeometry();
+    auto inScreenArea = [screenArea](const QRect &target, Qt::Edges edges = Qt::LeftEdge | Qt::RightEdge | Qt::TopEdge | Qt::BottomEdge) -> bool {
+        if (edges & Qt::LeftEdge && target.left() < screenArea.left()) {
+            return false;
+        }
+        if (edges & Qt::TopEdge && target.top() < screenArea.top()) {
+            return false;
+        }
+        if (edges & Qt::RightEdge && target.right() > screenArea.right()) {
+            return false;
+        }
+        if (edges & Qt::BottomEdge && target.bottom() > screenArea.bottom()) {
+            return false;
+        }
+        return true;
+    };
+
+    // if that fits, we don't need to do anything
+    if (inScreenArea(popupRect)) {
+        return popupRect;
+    }
+    // Otherwise,
+    if (placement.flipConstraintAdjustments() & Qt::Horizontal) {
+        if (!inScreenArea(popupRect, Qt::LeftEdge | Qt::RightEdge)) {
+            // flip both edges (if either bit is set, XOR both)
+            auto flippedParentAnchor = placement.parentAnchor();
+            if (flippedParentAnchor & (Qt::LeftEdge | Qt::RightEdge)) {
+                flippedParentAnchor ^= (Qt::LeftEdge | Qt::RightEdge);
+            }
+            auto flippedPopupAnchor = placement.popupAnchor();
+            if (flippedPopupAnchor & (Qt::LeftEdge | Qt::RightEdge)) {
+                flippedPopupAnchor ^= (Qt::LeftEdge | Qt::RightEdge);
+            }
+            QRect flippedPopupRect = QRect(popupPosition(globalParentAnchorRect, flippedParentAnchor, flippedPopupAnchor, w->size()), w->size());
+            // if it still doesn't fit we should continue with the unflipped version
+            if (inScreenArea(flippedPopupRect, Qt::LeftEdge | Qt::RightEdge)) {
+                popupRect.moveLeft(flippedPopupRect.left());
+            }
+        }
+    }
+    if (placement.slideConstraintAdjustments() & Qt::Horizontal) {
+        if (!inScreenArea(popupRect, Qt::LeftEdge)) {
+            popupRect.moveLeft(screenArea.left());
+        }
+        if (!inScreenArea(popupRect, Qt::RightEdge)) {
+            popupRect.moveRight(screenArea.right());
+        }
+    }
+    if (placement.flipConstraintAdjustments() & Qt::Vertical) {
+        if (!inScreenArea(popupRect, Qt::TopEdge | Qt::BottomEdge)) {
+            // flip both edges (if either bit is set, XOR both)
+            auto flippedParentAnchor = placement.parentAnchor();
+            if (flippedParentAnchor & (Qt::TopEdge | Qt::BottomEdge)) {
+                flippedParentAnchor ^= (Qt::TopEdge | Qt::BottomEdge);
+            }
+            auto flippedPopupAnchor = placement.popupAnchor();
+            if (flippedPopupAnchor & (Qt::TopEdge | Qt::BottomEdge)) {
+                flippedPopupAnchor ^= (Qt::TopEdge | Qt::BottomEdge);
+            }
+            QRect flippedPopupRect = QRect(popupPosition(globalParentAnchorRect, flippedParentAnchor, flippedPopupAnchor, w->size()), w->size());
+            // if it still doesn't fit we should continue with the unflipped version
+            if (inScreenArea(flippedPopupRect, Qt::TopEdge | Qt::BottomEdge)) {
+                popupRect.moveTop(flippedPopupRect.top());
+            }
+        }
+    }
+    if (placement.slideConstraintAdjustments() & Qt::Vertical) {
+        if (!inScreenArea(popupRect, Qt::TopEdge)) {
+            popupRect.moveTop(screenArea.top());
+        }
+        if (!inScreenArea(popupRect, Qt::BottomEdge)) {
+            popupRect.moveBottom(screenArea.bottom());
+        }
+    }
+    return popupRect;
+}

src/plasmaquick/transientplacementhint_p.h
@@ -1,0 +1,45 @@
+/*
+    SPDX-FileCopyrightText: 2023 David Edmundson <davidedmundson@kde.org>
+    SPDX-FileCopyrightText: 2021 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QRect>
+#include <QSharedDataPointer>
+#include <QtGlobal>
+
+// This class is proposed for Qt6.something, but it's not there yet.
+// keep as an implementation detail, and then drop eventually (famous last words)
+
+class TransientPlacementHintPrivate;
+class QWindow;
+
+class TransientPlacementHint
+{
+public:
+    TransientPlacementHint();
+    ~TransientPlacementHint();
+    TransientPlacementHint(const TransientPlacementHint &other);
+    TransientPlacementHint &operator=(const TransientPlacementHint &other);
+    bool isValid() const;
+    void setParentAnchorArea(const QRect &parentAnchorRect);
+    QRect parentAnchorArea() const;
+    void setParentAnchor(Qt::Edges parentAnchor);
+    Qt::Edges parentAnchor() const;
+    void setPopupAnchor(Qt::Edges popupAnchor);
+    Qt::Edges popupAnchor() const;
+    Qt::Orientations slideConstraintAdjustments() const;
+    void setSlideConstraintAdjustments(Qt::Orientations slideConstraintAdjustments);
+    Qt::Orientations flipConstraintAdjustments() const;
+    void setFlipConstraintAdjustments(Qt::Orientations flipConstraintAdjustments);
+
+private:
+    QSharedDataPointer<TransientPlacementHintPrivate> d;
+};
+
+namespace TransientPlacementHelper
+{
+QRect popupRect(QWindow *window, const TransientPlacementHint &placement);
+}

src/plasmaquick/waylandintegration_p.h
@@ -13,6 +13,8 @@
 
 #include "qwayland-plasma-shell.h"
 
+#include <plasmaquick/plasmaquick_export.h>
+
 class QWindow;
 
 class PlasmaShellSurface;
@@ -24,7 +26,8 @@ class PlasmaShellSurface;
  * The class abstracts the wayland protocol tasks, automatically sending
  * cached metadata when the underlying platform surfaces are created.
  */
-class PlasmaShellWaylandIntegration : public QObject
+
+class PLASMAQUICK_EXPORT PlasmaShellWaylandIntegration : public QObject
 {
     Q_OBJECT
 public: