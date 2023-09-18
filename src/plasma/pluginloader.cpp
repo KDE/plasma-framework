db5423255bfee18369c67b0c2260da552e458783
Nicolas Fella <nicolas.fella@gmx.de>
2023-09-18 20:53:40 +0200

    [pluginloader] Merge listAppletMetaData and listAppletMetaDataInternal

--- src/plasma/pluginloader.cpp
+++ src/plasma/pluginloader.cpp
@@ -152,7 +152,7 @@ ContainmentActions *PluginLoader::loadContainmentActions(Containment *parent, co
     return nullptr;
 }
 
-QList<KPluginMetaData> listAppletMetaDataInternal(const QString &category, const QString &parentApp)
+QList<KPluginMetaData> PluginLoader::listAppletMetaData(const QString &category)
 {
     auto platforms = KRuntimePlatform::runtimePlatform();
     // For now desktop always lists everything
@@ -209,11 +209,6 @@ QList<KPluginMetaData> listAppletMetaDataInternal(const QString &category, const
     }
 
     return KPackage::PackageLoader::self()->findPackages(QStringLiteral("Plasma/Applet"), QString(), filter);
-}
-
-QList<KPluginMetaData> PluginLoader::listAppletMetaData(const QString &category)
-{
-    return listAppletMetaDataInternal(category, QString());
 }
 
 QList<KPluginMetaData> PluginLoader::listAppletMetaDataForMimeType(const QString &mimeType)
