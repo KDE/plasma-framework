faf8f617a661caabc6f276b108aceb3e0e64296c
Marco Martin <notmart@gmail.com>
2023-11-17 09:06:07 +0000

    Different way to deduce systray's corona
    when unloading the shell due to look and feel change,
    as object get deleted from the parent to the children,
    the panel gets deleted and during its dtor before the
    children (the systray container and then the systray)
    get actually deleted, corona emits an
    availableScreenRect/RegionChanged, the systray containment
    searches for its own screen, but it can't find its corona
    anymore because a containment in the hierarchy is not
    a containment anymore but just a QObject
    
    BUG:477067

--- src/plasma/containment.cpp
+++ src/plasma/containment.cpp
@@ -298,13 +298,15 @@ Plasma::Containment::Type Containment::containmentType() const
 
 Corona *Containment::corona() const
 {
-    if (Plasma::Corona *corona = qobject_cast<Corona *>(parent())) {
-        return corona;
-        // case in which this containment is child of an applet, hello systray :)
-    } else {
-        Plasma::Applet *parentApplet = qobject_cast<Plasma::Applet *>(parent());
-        if (parentApplet && parentApplet->containment()) {
-            return parentApplet->containment()->corona();
+    // We are not sure where the corona parent is in the hyerarchy, because of... the systray.
+    // We are iterating over the parent tree here rather than casting the parent
+    // to applet then asking ofr its containment and corona, as this might break during
+    // teardown, as this can be invoked during dtor of one of the ancestors,
+    // see https://bugs.kde.org/show_bug.cgi?id=477067 where it happens during destruction
+    // of the panel (containment of the applet that contains the systray containment)
+    for (auto candidate = parent(); candidate; candidate = candidate->parent()) {
+        if (auto c = qobject_cast<Corona *>(candidate)) {
+            return c;
         }
     }
 
